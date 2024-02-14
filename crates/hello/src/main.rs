a2b877b976cff609ec0fe4659a8d99655a8bf98d
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-02-14 01:29:17 -0800

    basic tracing and logging

--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -1,33 +1,40 @@
 use opentelemetry::trace::TracerProvider as _;
 use opentelemetry_sdk::trace::TracerProvider;
 use opentelemetry_stdout as stdout;
-use tracing::{error, span};
-use tracing_subscriber::layer::SubscriberExt;
-use tracing_subscriber::Registry;
+use tracing::{error, error_span, span};
+use tracing_subscriber::{
+    fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry
+};
 
 fn main() {
     // Create a new OpenTelemetry trace pipeline that prints to stdout
     let provider = TracerProvider::builder()
         .with_simple_exporter(stdout::SpanExporter::default())
         .build();
-    let tracer = provider.tracer("readme_example");
+
+    let tracer = provider.tracer("hello_example");
 
     // Create a tracing layer with the configured tracer
-    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let telemetry = tracing_opentelemetry::layer()
+        .with_tracer(tracer);
 
-    // Use the tracing subscriber `Registry`, or any other subscriber
-    // that impls `LookupSpan`
-    let subscriber = Registry::default().with(telemetry);
+    let subscriber = Registry::default()
+        .with(telemetry)
+        .with(EnvFilter::from_default_env())
+        .with(fmt::layer().json())
+        .try_init();
 
-    // Trace executed code
-    tracing::subscriber::with_default(subscriber, || {
-        // Spans will be sent to the configured OpenTelemetry exporter
-        let root = span!(tracing::Level::TRACE, "app_start", work_units = 2);
+    {
+        let root = error_span!("Meow Meow");
         let _enter = root.enter();
+        error!("Meow Meow Meow I am Error Log in Trace"); // Works!!!!
+    }
 
-        error!("CHECK HERE ERROR"); // Works
-    });
-
-    println!("Hello, world!");
-    error!("Error Log: Hello, world!"); // Doesn't work
+    error!("Meow Meow Meow I am Normal Error Log"); // Works!!!!
 }
+
+// Important reading:
+// https://github.com/tokio-rs/tracing?tab=readme-ov-file#in-libraries
+
+// "Libraries should NOT call set_global_default()! That will cause conflicts when executables try to set them later."
+// https://docs.rs/tracing/latest/tracing/subscriber/fn.set_global_default.html
