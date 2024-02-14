5426d64a3525191d3221e472dbe1d4c1ebc7453c
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-02-13 23:11:15 -0800

    playing around with tracing and logging

--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -1,3 +1,33 @@
+use opentelemetry::trace::TracerProvider as _;
+use opentelemetry_sdk::trace::TracerProvider;
+use opentelemetry_stdout as stdout;
+use tracing::{error, span};
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::Registry;
+
 fn main() {
+    // Create a new OpenTelemetry trace pipeline that prints to stdout
+    let provider = TracerProvider::builder()
+        .with_simple_exporter(stdout::SpanExporter::default())
+        .build();
+    let tracer = provider.tracer("readme_example");
+
+    // Create a tracing layer with the configured tracer
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+
+    // Use the tracing subscriber `Registry`, or any other subscriber
+    // that impls `LookupSpan`
+    let subscriber = Registry::default().with(telemetry);
+
+    // Trace executed code
+    tracing::subscriber::with_default(subscriber, || {
+        // Spans will be sent to the configured OpenTelemetry exporter
+        let root = span!(tracing::Level::TRACE, "app_start", work_units = 2);
+        let _enter = root.enter();
+
+        error!("CHECK HERE ERROR"); // Works
+    });
+
     println!("Hello, world!");
+    error!("Error Log: Hello, world!"); // Doesn't work
 }
