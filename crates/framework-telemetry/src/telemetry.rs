1c001082b581a82dde7e3926380702847c1511e0
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-02-16 20:09:44 -0800

    basic tracing to stdout

--- crates/framework-telemetry/src/telemetry.rs
+++ crates/framework-telemetry/src/telemetry.rs
@@ -1,21 +1,32 @@
 use std::time::Duration;
 
-use opentelemetry::global;
-use opentelemetry::trace::TraceError;
-use opentelemetry_otlp::{self, WithExportConfig};
+use opentelemetry::{global, trace::TracerProvider, Key};
+
 use opentelemetry_sdk::{
     propagation::TraceContextPropagator,
     resource::{
         EnvResourceDetector, OsResourceDetector, ProcessResourceDetector, ResourceDetector,
         SdkProvidedResourceDetector, TelemetryResourceDetector,
     },
-    runtime, trace as sdktrace,
+    runtime,
+    trace::{
+        config, BatchSpanProcessor as SDKBatchSpanProcessor, Tracer as SDKTracer,
+        TracerProvider as SDKTracerProvider,
+    },
 };
 
-use tracing_subscriber::{fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry};
+use tracing_subscriber::{fmt, layer::SubscriberExt, Registry};
 
 pub fn init_telemetry() -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
-    // Methods to inject & extract text into injectors & extractors across process boundaries.
+    let tracer = init_tracer();
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default()
+        .with(fmt::layer()) // If we want JSON: fmt::layer().json().flatten_event(true)
+        .with(telemetry);
+    tracing::subscriber::set_global_default(subscriber)
+}
+
+pub fn init_tracer() -> SDKTracer {
     global::set_text_map_propagator(TraceContextPropagator::new());
 
     let os_resource = OsResourceDetector.detect(Duration::from_secs(0));
@@ -24,50 +35,24 @@ pub fn init_telemetry() -> Result<(), tracing::subscriber::SetGlobalDefaultError
     let env_resource = EnvResourceDetector::new().detect(Duration::from_secs(0));
     let telemetry_resource = TelemetryResourceDetector.detect(Duration::from_secs(0));
 
-    let subscriber = Registry::default().with(fmt::layer().json().flatten_event(true));
-    // .with(EnvFilter::from_default_env());
+    let exporter = opentelemetry_stdout::SpanExporter::default();
+    let processor = SDKBatchSpanProcessor::builder(exporter, runtime::Tokio).build();
+    let provider = SDKTracerProvider::builder()
+        .with_span_processor(processor)
+        // .with_simple_exporter(opentelemetry_stdout::SpanExporter::default())
+        .with_config(
+            config().with_resource(
+                os_resource
+                    .merge(&process_resource)
+                    .merge(&sdk_resource)
+                    .merge(&env_resource)
+                    .merge(&telemetry_resource),
+            ),
+        )
+        .build();
 
-    tracing::subscriber::set_global_default(subscriber)
+    provider.tracer("Test") // TODO: should be service.name
 }
 
-//
-// use opentelemetry::trace::TracerProvider as _;
-// use opentelemetry_sdk::trace::TracerProvider;
-// use opentelemetry_stdout as stdout;
-// use tracing::{error, error_span, span};
-// use tracing_subscriber::{
-//     fmt, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter, Registry
-// };
-//
-// fn main() {
-//     // Create a new OpenTelemetry trace pipeline that prints to stdout
-//     let provider = TracerProvider::builder()
-//         .with_simple_exporter(stdout::SpanExporter::default())
-//         .build();
-//
-//     let tracer = provider.tracer("hello_example");
-//
-//     // Create a tracing layer with the configured tracer
-//     let framework-telemetry = tracing_opentelemetry::layer()
-//         .with_tracer(tracer);
-//
-//     let subscriber = Registry::default()
-//         .with(framework-telemetry)
-//         .with(EnvFilter::from_default_env())
-//         .with(fmt::layer().json())
-//         .try_init();
-//
-//     {
-//         let root = error_span!("Meow Meow");
-//         let _enter = root.enter();
-//         error!("Meow Meow Meow I am Error Log in Trace"); // Works!!!!
-//     }
-//
-//     error!("Meow Meow Meow I am Normal Error Log"); // Works!!!!
-// }
-
-// Important reading:
-// https://github.com/tokio-rs/tracing?tab=readme-ov-file#in-libraries
-
-// "Libraries should NOT call set_global_default()! That will cause conflicts when executables try to set them later."
-// https://docs.rs/tracing/latest/tracing/subscriber/fn.set_global_default.html
+// TODO: init_metrics
+pub fn init_metrics() {}
--- crates/hello/src/main.rs
+++ crates/hello/src/main.rs
@@ -1,7 +1,53 @@
 use framework_telemetry::telemetry::init_telemetry;
-use tracing::{error, error_span, info, span};
+use std::{thread, time::Duration};
+use tracing::{error, error_span, info, info_span, instrument, span, trace, warn};
 
-fn main() {
+#[instrument]
+#[inline]
+fn expensive_work() -> &'static str {
+    span!(tracing::Level::INFO, "expensive_step_1")
+        .in_scope(|| thread::sleep(Duration::from_millis(25)));
+    span!(tracing::Level::INFO, "expensive_step_2")
+        .in_scope(|| thread::sleep(Duration::from_millis(25)));
+
+    "success"
+}
+
+#[tokio::main]
+async fn main() {
     init_telemetry();
-    info!("Meow Meow Meow");
+
+    info!(
+        meow_1 = "Meow1",
+        meow_2 = "Meow2",
+        "Info Log: Meow Meow Meow"
+    );
+    error!(
+        meow_1 = "Meow1",
+        meow_2 = "Meow2",
+        "Error Log: Meow Meow Meow"
+    );
+
+    {
+        let root = error_span!("Error Span: Meow Meow");
+        let _enter = root.enter();
+        error!("Meow Meow Meow I am Error Log in Error Span");
+        trace!("Trace: ");
+    }
+
+    {
+        let root = span!(tracing::Level::INFO, "app_start", work_units = 2);
+        let _enter = root.enter();
+
+        let work_result = expensive_work();
+
+        span!(tracing::Level::INFO, "Info Span: Meow!!!!")
+            .in_scope(|| thread::sleep(Duration::from_millis(10)));
+
+        info_span!("Info Span: Meow");
+        error_span!("Error Span: Meow");
+
+        warn!("Warn Log: About to exit!");
+        trace!("status: {}", work_result);
+    }
 }
