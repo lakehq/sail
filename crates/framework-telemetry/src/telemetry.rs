56434a2176a1917c16ce6a4898dd86aab32882c3
Shehab Amin <11789402+shehabgamin@users.noreply.github.com>
2024-02-16 20:45:22 -0800

    Initial OpenTelemetry Tracer to stdout (#4)

---
+++ crates/framework-telemetry/src/telemetry.rs
@@ -0,0 +1,57 @@
+use std::time::Duration;
+
+use opentelemetry::{global, trace::TracerProvider, Key};
+
+use opentelemetry_sdk::{
+    propagation::TraceContextPropagator,
+    resource::{
+        EnvResourceDetector, OsResourceDetector, ProcessResourceDetector, ResourceDetector,
+        SdkProvidedResourceDetector, TelemetryResourceDetector,
+    },
+    runtime,
+    trace::{
+        config, BatchSpanProcessor as SDKBatchSpanProcessor, Tracer as SDKTracer,
+        TracerProvider as SDKTracerProvider,
+    },
+};
+
+use tracing_subscriber::{fmt, layer::SubscriberExt, Registry};
+
+pub fn init_telemetry() -> Result<(), tracing::subscriber::SetGlobalDefaultError> {
+    let tracer = init_tracer();
+    let telemetry = tracing_opentelemetry::layer().with_tracer(tracer);
+    let subscriber = Registry::default()
+        .with(fmt::layer()) // If we want JSON: fmt::layer().json().flatten_event(true)
+        .with(telemetry);
+    tracing::subscriber::set_global_default(subscriber)
+}
+
+pub fn init_tracer() -> SDKTracer {
+    global::set_text_map_propagator(TraceContextPropagator::new());
+
+    let os_resource = OsResourceDetector.detect(Duration::from_secs(0));
+    let process_resource = ProcessResourceDetector.detect(Duration::from_secs(0));
+    let sdk_resource = SdkProvidedResourceDetector.detect(Duration::from_secs(0));
+    let env_resource = EnvResourceDetector::new().detect(Duration::from_secs(0));
+    let telemetry_resource = TelemetryResourceDetector.detect(Duration::from_secs(0));
+
+    let exporter = opentelemetry_stdout::SpanExporter::default();
+    let processor = SDKBatchSpanProcessor::builder(exporter, runtime::Tokio).build();
+    let provider = SDKTracerProvider::builder()
+        .with_span_processor(processor)
+        .with_config(
+            config().with_resource(
+                os_resource
+                    .merge(&process_resource)
+                    .merge(&sdk_resource)
+                    .merge(&env_resource)
+                    .merge(&telemetry_resource),
+            ),
+        )
+        .build();
+
+    provider.tracer("Test") // TODO: should be service.name
+}
+
+// TODO: init_metrics
+pub fn init_metrics() {}
