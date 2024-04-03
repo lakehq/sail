6b25882bda15856e233d13fcf1103a2c33f9cfe6
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-04-02 22:20:02 -0700

    save

--- crates/spark-connect-server/src/main.rs
+++ crates/spark-connect-server/src/main.rs
@@ -7,6 +7,29 @@ use framework_telemetry::telemetry::init_telemetry;
 use spark_connect_server::server::SparkConnectServer;
 use spark_connect_server::spark::connect::spark_connect_service_server::SparkConnectServiceServer;
 
+use pyo3::prelude::*;
+use pyo3::types::IntoPyDict;
+
+// fn main() -> PyResult<()> {
+//     Python::with_gil(|py| {
+//         let sys = py.import_bound("sys")?;
+//         let version: String = sys.getattr("version")?.extract()?;
+//
+//         let locals = [("os", py.import_bound("os")?)].into_py_dict_bound(py);
+//         let code = "os.getenv('USER') or os.getenv('USERNAME') or 'Unknown'";
+//         let user: String = py.eval_bound(code, None, Some(&locals))?.extract()?;
+//
+//         println!("Hello {}, I'm Python {}", user, version);
+//
+//         let cloudpickle = PyModule::import_bound(py, "pyspark.cloudpickle")
+//             .expect("Unable to import 'pyspark.cloudpickle'")
+//             .getattr("loads")
+//             .unwrap();
+//
+//         Ok(())
+//     })
+// }
+
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
     init_telemetry()?;
