a6d99b1a8bc291b534d5d9fff6bd7d6c5b7b8a04
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-04-30 23:46:28 -0700

    Add pyspark build

--- scripts/spark-connect-server/setup-spark-env.sh
+++ scripts/spark-connect-server/setup-spark-env.sh
@@ -7,6 +7,9 @@ cd "${SPARK_PROJECT_PATH}" || exit
 git checkout v3.5.1 && git add . && git stash
 git apply "${FRAMEWORK_PROJECT_PATH}"/scripts/spark-connect-server/spark-3.5.1.patch
 
+# Build the Spark project.
+./build/mvn -DskipTests clean package
+
 # Create a directory for test logs. This directory is in `.gitignore`.
 mkdir -p logs
 
