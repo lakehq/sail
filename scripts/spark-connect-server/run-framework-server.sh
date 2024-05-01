de95df94f83785d1c72831014fb8a3af9000b396
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-04-30 23:14:30 -0700

    use full path

--- scripts/spark-connect-server/run-framework-server.sh
+++ scripts/spark-connect-server/run-framework-server.sh
@@ -4,12 +4,12 @@ set -euo 'pipefail'
 
 cd "${FRAMEWORK_PROJECT_PATH}" || exit
 
-source examples/python/.venv/bin/activate
+source "${FRAMEWORK_PROJECT_PATH}"/examples/python/.venv/bin/activate
 
 export RUST_LOG=spark_connect_server=debug
 export RUST_BACKTRACE=full
 # We have to set `PYTHONPATH` even if we are using the virtual environment.
 # This is because the Python executable is the Rust program itself, and there is
 # no `pyvenv.cfg` at its required location (one directory above the executable).
-export PYTHONPATH="examples/python/.venv/lib/python3.11/site-packages"
+export PYTHONPATH="${FRAMEWORK_PROJECT_PATH}/examples/python/.venv/lib/python3.11/site-packages"
 cargo run -p spark-connect-server
