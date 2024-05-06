b50dd5b4a0f41c084567851404620e48e01a70d0
Heran Lin <heran@lakesail.com>
2024-05-06 20:21:59 +0800

    Fix

--- scripts/spark-tests/setup-spark-env.sh
+++ scripts/spark-tests/setup-spark-env.sh
@@ -7,7 +7,11 @@ project_path="$(dirname "$0")/../.."
 cd "${project_path}"/opt/spark
 
 # Build the Spark project.
-./build/mvn -Pconnect -Phive -DskipTests clean package
+# We do not include the "clean" goal so that the build output can be reused.
+./build/mvn \
+  --batch-mode \
+  -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
+  -Pconnect -Phive -DskipTests package
 
 # Create a directory for test logs. This directory is in `.gitignore`.
 mkdir -p logs
