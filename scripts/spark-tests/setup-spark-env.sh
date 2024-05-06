1344643e7630ebde17f1bdcb09870bba7f40242e
Heran Lin <heran@lakesail.com>
2024-05-06 22:07:22 +0800

    Update

--- scripts/spark-tests/setup-spark-env.sh
+++ scripts/spark-tests/setup-spark-env.sh
@@ -11,7 +11,6 @@ cd "${project_path}"/opt/spark
 ./build/mvn \
   --batch-mode \
   -Dorg.slf4j.simpleLogger.log.org.apache.maven.cli.transfer.Slf4jMavenTransferListener=warn \
-  -Dmaven.test.skip \
   -DskipTests \
   -Phive package
 
