796cfcfaa820e766fe758ccfc2d3fb5f4d91b56a
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-05-15 05:54:22 -0700

    [spark tests]

--- scripts/spark-tests/run-server.sh
+++ scripts/spark-tests/run-server.sh
@@ -10,6 +10,7 @@ python_version=$(python -c 'import sys; print("%s.%s" % (sys.version_info.major,
 
 export RUST_LOG="${RUST_LOG:-spark_connect_server=debug}"
 export RUST_BACKTRACE="${RUST_BACKTRACE:-full}"
+export RUST_MIN_STACK="${RUST_MIN_STACK:-8388608}"
 # We have to set `PYTHONPATH` even if we are using the virtual environment.
 # This is because the Python executable is the Rust program itself, and there is
 # no `pyvenv.cfg` at its required location (one directory above the executable).
