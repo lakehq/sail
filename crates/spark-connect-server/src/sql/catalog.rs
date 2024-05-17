ac3ec594fd63647d6723645066e74ea85e4cca2e
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-05-16 20:10:13 -0700

    catalog table

--- crates/spark-connect-server/src/sql/catalog.rs
+++ crates/spark-connect-server/src/sql/catalog.rs
@@ -1,9 +1,11 @@
 use std::sync::Arc;
 
 use crate::error::SparkResult;
-use datafusion::arrow::array::{RecordBatch, StringArray};
+use datafusion::arrow::array::{
+    BooleanArray, GenericListBuilder, GenericStringBuilder, ListBuilder, RecordBatch, StringArray,
+    StringBuilder,
+};
 use datafusion::arrow::datatypes::{DataType, Field, Schema, SchemaRef};
-use datafusion::catalog::CatalogProviderList;
 use datafusion::datasource::MemTable;
 
 #[derive(Debug, Clone)]
@@ -57,7 +59,11 @@ impl CatalogTable {
         SchemaRef::new(Schema::new(vec![
             Field::new("name", DataType::Utf8, false),
             Field::new("catalog", DataType::Utf8, true),
-            Field::new("namespace", DataType::Utf8, true),
+            Field::new_list(
+                "namespace",
+                Field::new_list_field(DataType::Utf8, false),
+                true,
+            ),
             Field::new("description", DataType::Utf8, true),
             Field::new("table_type", DataType::Utf8, false),
             Field::new("is_temporary", DataType::Boolean, false),
@@ -113,6 +119,53 @@ pub(crate) fn create_catalog_database_memtable(
             Arc::new(StringArray::from(catalogs)),
             Arc::new(StringArray::from(descriptions)),
             Arc::new(StringArray::from(location_uris)),
+        ],
+    )?;
+
+    Ok(MemTable::try_new(schema_ref, vec![vec![record_batch]])?)
+}
+
+pub(crate) fn create_catalog_table_memtable(tables: Vec<CatalogTable>) -> SparkResult<MemTable> {
+    let schema_ref = CatalogDatabase::schema();
+
+    let mut names: Vec<String> = Vec::with_capacity(tables.len());
+    let mut catalogs: Vec<Option<String>> = Vec::with_capacity(tables.len());
+    // let mut namespaces: Vec<Option<Vec<String>>> = Vec::with_capacity(tables.len());
+    let mut namespaces_builder: GenericListBuilder<i32, GenericStringBuilder<i32>> =
+        ListBuilder::with_capacity(StringBuilder::new(), tables.len());
+    let mut descriptions: Vec<Option<String>> = Vec::with_capacity(tables.len());
+    let mut table_types: Vec<String> = Vec::with_capacity(tables.len());
+    let mut is_temporary: Vec<bool> = Vec::with_capacity(tables.len());
+
+    for table in tables {
+        names.push(table.name);
+        catalogs.push(table.catalog);
+
+        match table.namespace {
+            Some(namespace_list) => {
+                for namespace in namespace_list {
+                    namespaces_builder.values().append_value(&namespace);
+                }
+                namespaces_builder.append(true);
+            }
+            None => namespaces_builder.append(false),
+        }
+
+        descriptions.push(table.description);
+        table_types.push(table.table_type);
+        is_temporary.push(table.is_temporary);
+    }
+    let namespaces = namespaces_builder.finish();
+
+    let record_batch = RecordBatch::try_new(
+        schema_ref.clone(),
+        vec![
+            Arc::new(StringArray::from(names)),
+            Arc::new(StringArray::from(catalogs)),
+            Arc::new(namespaces),
+            Arc::new(StringArray::from(descriptions)),
+            Arc::new(StringArray::from(table_types)),
+            Arc::new(BooleanArray::from(is_temporary)),
         ],
     )?;
 
