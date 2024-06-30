ee722b9489ea061ff20a50540a99d5fd22df3067
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-06-30 05:44:47 -0700

    [spark tests]

--- crates/framework-sql/src/plan.rs
+++ crates/framework-sql/src/plan.rs
@@ -91,9 +91,41 @@ fn from_ast_statement(statement: ast::Statement) -> SqlResult<spec::Plan> {
 
     match statement {
         Statement::Query(query) => from_ast_query(*query),
-        Statement::Insert(_) => Err(SqlError::todo("SQL insert")),
-        Statement::Call(_) => Err(SqlError::todo("SQL call")),
-        Statement::Copy { .. } => Err(SqlError::todo("SQL copy")),
+        Statement::Insert(ast::Insert {
+            or: _,
+            ignore: _,
+            into: _,
+            table_name: _,
+            table_alias: _,
+            columns: _,
+            overwrite: _,
+            source: _,
+            partitioned: _,
+            after_columns: _,
+            table: _,
+            on: _,
+            returning: _,
+            replace_into: _,
+            priority: _,
+            insert_alias: _,
+        }) => Err(SqlError::todo("SQL insert")),
+        Statement::Call(ast::Function {
+            name: _,
+            parameters: _,
+            args: _,
+            filter: _,
+            null_treatment: _,
+            over: _,
+            within_group: _,
+        }) => Err(SqlError::todo("SQL call")),
+        Statement::Copy {
+            source: _,
+            to: _,
+            target: _,
+            options: _,
+            legacy_options: _,
+            values: _,
+        } => Err(SqlError::todo("SQL copy")),
         Statement::Explain {
             describe_alias: _,
             analyze,
@@ -124,9 +156,28 @@ fn from_ast_statement(statement: ast::Statement) -> SqlResult<spec::Plan> {
             };
             Ok(spec::Plan::new(node))
         }
-        Statement::AlterTable { .. } => Err(SqlError::todo("SQL alter table")),
-        Statement::AlterView { .. } => Err(SqlError::todo("SQL alter view")),
-        Statement::Analyze { .. } => Err(SqlError::todo("SQL analyze")),
+        Statement::AlterTable {
+            name: _,
+            if_exists: _,
+            only: _,
+            operations: _,
+            location: _,
+        } => Err(SqlError::todo("SQL alter table")),
+        Statement::AlterView {
+            name: _,
+            columns: _,
+            query: _,
+            with_options: _,
+        } => Err(SqlError::todo("SQL alter view")),
+        Statement::Analyze {
+            table_name: _,
+            partitions: _,
+            for_columns: _,
+            columns: _,
+            cache_metadata: _,
+            noscan: _,
+            compute_statistics: _,
+        } => Err(SqlError::todo("SQL analyze")),
         Statement::CreateDatabase {
             db_name,
             if_not_exists,
@@ -147,68 +198,116 @@ fn from_ast_statement(statement: ast::Statement) -> SqlResult<spec::Plan> {
             };
             Ok(spec::Plan::new(node))
         }
-        Statement::CreateFunction { .. } => Err(SqlError::todo("SQL create function")),
-        Statement::CreateIndex { .. } => Err(SqlError::todo("SQL create index")),
-        Statement::CreateSchema { .. } => Err(SqlError::todo("SQL create schema")),
-        Statement::CreateTable(_create_table) => {
-            // let ast::CreateTable {
-            //     or_replace,
-            //     temporary: _,
-            //     external: _,
-            //     global: _,
-            //     if_not_exists,
-            //     transient: _,
-            //     volatile: _,
-            //     name,
-            //     columns,
-            //     constraints,
-            //     hive_distribution: _,
-            //     hive_formats: _,
-            //     table_properties,
-            //     with_options,
-            //     file_format: _,
-            //     location: _,
-            //     query,
-            //     without_rowid: _,
-            //     like: _,
-            //     clone: _,
-            //     engine: _,
-            //     comment: _,
-            //     auto_increment_offset: _,
-            //     default_charset: _,
-            //     collation: _,
-            //     on_commit: _,
-            //     on_cluster: _,
-            //     primary_key: _,
-            //     order_by: _,
-            //     partition_by: _,
-            //     cluster_by: _,
-            //     options: _,
-            //     strict: _,
-            //     copy_grants: _,
-            //     enable_schema_evolution: _,
-            //     change_tracking: _,
-            //     data_retention_time_in_days: _,
-            //     max_data_extension_time_in_days: _,
-            //     default_ddl_collation: _,
-            //     with_aggregation_policy: _,
-            //     with_row_access_policy: _,
-            //     with_tags: _,
-            // } = create_table;
-            //
-            // if !table_properties.is_empty() {
-            //     return Err(SqlError::todo(format!(
-            //         "Statement::CreateTable table_properties: {:?}",
-            //         table_properties
-            //     )));
-            // }
-            // if !with_options.is_empty() {
-            //     return Err(SqlError::todo(format!(
-            //         "Statement::CreateTable with_options: {:?}",
-            //         with_options
-            //     )));
-            // }
-            //
+        Statement::CreateSchema {
+            schema_name,
+            if_not_exists,
+        } => {
+            let db_name = match schema_name {
+                ast::SchemaName::Simple(object_name) => from_ast_object_name(object_name)?,
+                ast::SchemaName::UnnamedAuthorization(ident) => {
+                    from_ast_object_name(ast::ObjectName(vec![ident]))?
+                }
+                ast::SchemaName::NamedAuthorization(object_name, ident) => {
+                    let mut object_name_parts = object_name.0;
+                    object_name_parts.push(ident);
+                    from_ast_object_name(ast::ObjectName(object_name_parts))?
+                }
+            };
+            let node = spec::PlanNode::CreateDatabase {
+                database: db_name,
+                if_not_exists,
+                comment: None,
+                location: None,
+                properties: Default::default(), // TODO: support properties
+            };
+            Ok(spec::Plan::new(node))
+        }
+        Statement::CreateFunction {
+            or_replace: _,
+            temporary: _,
+            if_not_exists: _,
+            name: _,
+            args: _,
+            return_type: _,
+            function_body: _,
+            behavior: _,
+            called_on_null: _,
+            parallel: _,
+            using: _,
+            language: _,
+            determinism_specifier: _,
+            options: _,
+            remote_connection: _,
+        } => Err(SqlError::todo("SQL create function")),
+        Statement::CreateIndex(ast::CreateIndex {
+            name: _,
+            table_name: _,
+            using: _,
+            columns: _,
+            unique: _,
+            concurrently: _,
+            if_not_exists: _,
+            include: _,
+            nulls_distinct: _,
+            predicate: _,
+        }) => Err(SqlError::todo("SQL create index")),
+        Statement::CreateTable(ast::CreateTable {
+            or_replace: _, // Using
+            temporary: _,
+            external: _,
+            global: _,
+            if_not_exists: _, // Using
+            transient: _,
+            volatile: _,
+            name: _,        // Using
+            columns: _,     // Using
+            constraints: _, // Using
+            hive_distribution: _,
+            hive_formats: _,
+            table_properties, // Using
+            with_options,     // Using
+            file_format: _,
+            location: _,
+            query: _, // Using
+            without_rowid: _,
+            like: _,
+            clone: _,
+            engine: _,
+            comment: _,
+            auto_increment_offset: _,
+            default_charset: _,
+            collation: _,
+            on_commit: _,
+            on_cluster: _,
+            primary_key: _,
+            order_by: _,
+            partition_by: _,
+            cluster_by: _,
+            options: _,
+            strict: _,
+            copy_grants: _,
+            enable_schema_evolution: _,
+            change_tracking: _,
+            data_retention_time_in_days: _,
+            max_data_extension_time_in_days: _,
+            default_ddl_collation: _,
+            with_aggregation_policy: _,
+            with_row_access_policy: _,
+            with_tags: _,
+        }) => {
+            if !table_properties.is_empty() {
+                return Err(SqlError::todo(format!(
+                    "Statement::CreateTable table_properties: {:?}",
+                    table_properties
+                )));
+            }
+            if !with_options.is_empty() {
+                return Err(SqlError::todo(format!(
+                    "Statement::CreateTable with_options: {:?}",
+                    with_options
+                )));
+            }
+
             // let node = spec::PlanNode::CreateTable {
             //     table: from_ast_object_name(name)?,
             //     path,
@@ -220,8 +319,29 @@ fn from_ast_statement(statement: ast::Statement) -> SqlResult<spec::Plan> {
             // Ok(spec::Plan::new(node))
             Err(SqlError::todo("SQL create table"))
         }
-        Statement::CreateView { .. } => Err(SqlError::todo("SQL create view")),
-        Statement::Delete(_) => Err(SqlError::todo("SQL delete")),
+        Statement::CreateView {
+            or_replace: _,
+            materialized: _,
+            name: _,
+            columns: _,
+            query: _,
+            options: _,
+            cluster_by: _,
+            comment: _,
+            with_no_schema_binding: _,
+            if_not_exists: _,
+            temporary: _,
+            to: _,
+        } => Err(SqlError::todo("SQL create view")),
+        Statement::Delete(ast::Delete {
+            tables: _,
+            from: _,
+            using: _,
+            selection: _,
+            returning: _,
+            order_by: _,
+            limit: _,
+        }) => Err(SqlError::todo("SQL delete")),
         Statement::Drop {
             object_type,
             if_exists,
