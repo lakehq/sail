a7e2e1d7942013133312e43e306d7d05639a37db
Heran Lin <heran@lakesail.com>
2024-07-03 09:36:15 +0800

    Address comments

--- crates/framework-python/src/udf/pyspark_udf.rs
+++ crates/framework-python/src/udf/pyspark_udf.rs
@@ -13,7 +13,8 @@ use crate::cereal::partial_pyspark_udf::PartialPySparkUDF;
 use crate::cereal::{is_pyspark_arrow_udf, is_pyspark_pandas_udf, PY_SPARK_SQL_BATCHED_UDF};
 use crate::udf::{
     build_pyarrow_array_kwargs, get_pyarrow_array_function, get_pyarrow_output_data_type,
-    get_python_builtins_list_function, get_python_builtins_str_function, PythonFunctionType,
+    get_python_builtins_list_function, get_python_builtins_str_function, get_udf_name,
+    PythonFunctionType,
 };
 
 #[derive(Debug, Clone)]
@@ -35,16 +36,7 @@ impl PySparkUDF {
         python_function: PartialPySparkUDF,
         output_type: DataType,
     ) -> Self {
-        // We generate a unique function name with the memory address of the Python function.
-        // Otherwise, lambda functions with the name "<lambda>" will be treated as the same function
-        // by logical plan optimization rules (e.g. common sub-expression elimination), resulting in
-        // incorrect logical plans.
-        // TODO: Apply this to all types of Python UDFs.
-        let function_name = format!(
-            "{}@0x{:x}",
-            function_name,
-            python_function.0.as_ptr() as usize
-        );
+        let function_name = get_udf_name(&function_name, &python_function.0);
         Self {
             signature: Signature::exact(
                 input_types,
