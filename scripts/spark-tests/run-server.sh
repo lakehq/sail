d2357283a548ad7914b9d9fa33ab40609c7d8092
Heran Lin <heran@lakesail.com>
2024-08-01 13:05:11 +0800

    Move Spark Connect server to examples

--- scripts/spark-tests/run-server.sh
+++ scripts/spark-tests/run-server.sh
@@ -5,9 +5,9 @@ set -euo 'pipefail'
 source "$(dirname "$0")/prepare-server.sh"
 
 if [[ -z "${BENCHMARK:-}" ]]; then
-  cargo run -p framework-spark-connect
+  cargo run -p framework-spark-connect --example server
 else
   # We build for the current CPU to get the best performance.
   # See also: https://crates.io/crates/arrow
-  env RUSTFLAGS="-C target-cpu=native" cargo run -p framework-spark-connect -r
+  env RUSTFLAGS="-C target-cpu=native" cargo run -p framework-spark-connect --example server -r
 fi
