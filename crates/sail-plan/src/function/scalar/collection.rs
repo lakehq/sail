c59ff5347094b8e327868254475d33af623c9752
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-08-06 23:12:56 -0700

    [spark tests]

--- crates/sail-plan/src/function/scalar/collection.rs
+++ crates/sail-plan/src/function/scalar/collection.rs
@@ -1,13 +1,22 @@
+use datafusion::functions_array::expr_fn;
+use datafusion_expr::expr;
+
 use crate::function::common::Function;
+
+fn concat(array1: expr::Expr, array2: expr::Expr) -> expr::Expr {
+    expr_fn::array_concat(vec![array1, array2])
+}
 
 pub(super) fn list_built_in_collection_functions() -> Vec<(&'static str, Function)> {
     use crate::function::common::FunctionBuilder as F;
 
     vec![
-        ("array_size", F::unknown("array_size")),
-        ("cardinality", F::unknown("cardinality")),
-        ("concat", F::unknown("concat")),
-        ("reverse", F::unknown("reverse")),
-        ("size", F::unknown("size")),
+        // TODO: Write tests for `array_size`, `cardinality` `size` to validate that the behavior
+        //  of expr_fn::cardinality fully matches behavior of Spark for all three.
+        ("array_size", F::unary(expr_fn::cardinality)),
+        ("cardinality", F::unary(expr_fn::cardinality)),
+        ("size", F::unary(expr_fn::cardinality)),
+        ("concat", F::binary(concat)),
+        ("reverse", F::unary(expr_fn::array_reverse)),
     ]
 }
