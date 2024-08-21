0e010d6cd1205f509f9764809e66c00ea2a7772f
Heran Lin <heran@lakesail.com>
2024-08-21 10:36:07 +0800

    Update README for the docs site launch

--- README.md
+++ README.md
@@ -14,14 +14,21 @@ Sail is available as a Python package on PyPI. You can install it using `pip`.
 pip install pysail
 ```
 
+You can follow the [Getting Started](https://docs.lakesail.com/sail/latest/guide/getting-started/) guide to learn more about Sail.
+
 **Note**: Currently only a source-only pre-release is available, so you need the Rust toolchain to install Sail.
 A proper release with pre-built wheels will be available soon.
 
 ## Documentation
 
-Please refer to the `docs` directory for the documentation.
+The documentation of the latest Sail version can be found [here](https://docs.lakesail.com/sail/latest/).
 
 ## Contributing
 
 Contributions are welcome!
+
 Please submit GitHub issues for bug reports and feature requests.
+
+Feel free to create a pull request if you would like to make a code change.
+You can refer to the `main` branch [development guide](https://docs.lakesail.com/sail/main/development/) to get started.
+Note that the development guide in the latest released version may not be up-to-date.
--- docs/.vitepress/theme/layouts/MainLayout.vue
+++ docs/.vitepress/theme/layouts/MainLayout.vue
@@ -7,7 +7,10 @@
     </template>
     <template #doc-before>
       <div class="vp-doc external-link-icon-enabled">
-        <div v-if="version !== 'latest'" class="warning custom-block py-4">
+        <div
+          v-if="version !== 'latest' && !isDevelopment"
+          class="warning custom-block py-4"
+        >
           <p>
             This is
             {{
@@ -19,17 +22,23 @@
           </p>
         </div>
         <div
-          v-if="
-            version !== 'main' &&
-            route.data.relativePath.startsWith('development/')
-          "
-          class="info custom-block py-4"
+          v-if="version !== 'main' && isDevelopment"
+          class="warning custom-block py-4"
         >
           <p>
             This is a snapshot of the development guide for a released Sail
             version. For up-to-date information, please visit the development
             guide for the <code>main</code> branch
             <a :href="`${mainBase}development/`" target="_blank"> here</a>.
+          </p>
+        </div>
+        <div
+          v-if="version === 'main' && isDevelopment"
+          class="info custom-block py-4"
+        >
+          <p>
+            This guide is up-to-date with the
+            <code>main</code> branch.
           </p>
         </div>
       </div>
@@ -48,6 +57,9 @@ const route = useRoute();
 
 const version = computed(() => site.value.contentProps?.version);
 const libVersion = computed(() => site.value.contentProps?.libVersion);
+const isDevelopment = computed(() =>
+  route.data.relativePath.startsWith("development/"),
+);
 
 const switchBase = (base: string, version: string): string => {
   return base.replace(/\/[^/]+\/$/, `/${version}/`);
