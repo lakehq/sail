6ac1c01fd8b76d46a6f3b8b414f6e0fde476c052
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-09-10 16:40:49 -0700

    cleanup

--- crates/sail-telemetry/src/telemetry.rs
+++ crates/sail-telemetry/src/telemetry.rs
@@ -1,51 +1,56 @@
+use std::borrow::Cow;
 use std::env;
 use std::io::Write;
 use std::time::Duration;
 
 use fastrace::collector::{Config, Reporter, SpanRecord};
-use fastrace_opentelemetry::OpenTelemetryReporter;
 use fastrace::prelude::*;
-// use log::error;
-use crate::error::TelemetryResult;
-
-
-use opentelemetry::global;
-use opentelemetry::trace::{TraceError, TracerProvider};
+use fastrace_opentelemetry::OpenTelemetryReporter;
+use opentelemetry::trace::SpanKind;
+use opentelemetry::{InstrumentationLibrary, KeyValue};
 use opentelemetry_otlp::{self, WithExportConfig};
-use opentelemetry_sdk::propagation::TraceContextPropagator;
-use opentelemetry_sdk::resource::{
-    EnvResourceDetector, ResourceDetector, SdkProvidedResourceDetector, TelemetryResourceDetector,
-};
-use opentelemetry_sdk::{runtime, trace as sdktrace};
+use opentelemetry_sdk::Resource;
+
+use crate::error::TelemetryResult;
 
 pub fn init_telemetry() -> TelemetryResult<()> {
     let use_collector = match env::var("SAIL_OPENTELEMETRY_COLLECTOR") {
         Ok(val) => !val.is_empty(),
         Err(_) => false,
     };
-    fastrace::set_reporter(
-        DummyReporter,
-        Config::default().report_before_root_finish(true),
-    );
     init_tracer(use_collector)?;
     init_logger(use_collector)?;
     Ok(())
 }
 
 pub fn init_tracer(use_collector: bool) -> TelemetryResult<()> {
+    let reporter_config = Config::default().report_before_root_finish(true);
     if use_collector {
-        let sdk_resource = SdkProvidedResourceDetector.detect(Duration::from_secs(0));
-        let env_resource = EnvResourceDetector::new().detect(Duration::from_secs(0));
-        let telemetry_resource = TelemetryResourceDetector.detect(Duration::from_secs(0));
-        fastrace::set_reporter(
-            DummyReporter,
-            Config::default().report_before_root_finish(true),
-        )
+        let host = env::var("SAIL_OPENTELEMETRY_COLLECTOR_SERVICE_HOST")?;
+        let port = env::var("SAIL_OPENTELEMETRY_COLLECTOR_SERVICE_PORT_OTLP_GRPC")?;
+        let url = format!("http://{host}:{port}");
+        let reporter = OpenTelemetryReporter::new(
+            opentelemetry_otlp::new_exporter()
+                .tonic()
+                .with_endpoint(url)
+                .with_protocol(opentelemetry_otlp::Protocol::Grpc)
+                .with_timeout(Duration::from_secs(
+                    opentelemetry_otlp::OTEL_EXPORTER_OTLP_TIMEOUT_DEFAULT,
+                ))
+                .build_span_exporter()
+                .expect("initialize oltp exporter"),
+            SpanKind::Server,
+            Cow::Owned(Resource::new([KeyValue::new(
+                "service.name",
+                "sail_server",
+            )])),
+            InstrumentationLibrary::builder("sail")
+                .with_version(env!("CARGO_PKG_VERSION"))
+                .build(),
+        );
+        fastrace::set_reporter(reporter, Config::default());
     } else {
-        fastrace::set_reporter(
-            DummyReporter,
-            Config::default().report_before_root_finish(true),
-        )
+        fastrace::set_reporter(DummyReporter, reporter_config)
     };
     Ok(())
 }
@@ -77,47 +82,5 @@ pub fn init_logger(use_collector: bool) -> TelemetryResult<()> {
 
 pub struct DummyReporter;
 impl Reporter for DummyReporter {
-    fn report(&mut self, _spans: Vec<SpanRecord>) {
-    }
+    fn report(&mut self, _spans: Vec<SpanRecord>) {}
 }
-
-// pub fn init_tracer() -> Result<sdktrace::Tracer, TelemetryError> {
-//     global::set_text_map_propagator(TraceContextPropagator::new());
-//
-//
-//     let trace_config = sdktrace::Config::default()
-//         .with_resource(sdk_resource.merge(&env_resource).merge(&telemetry_resource));
-//
-//     let use_collector = match env::var("SAIL_OPENTELEMETRY_COLLECTOR") {
-//         Ok(val) => !val.is_empty(),
-//         Err(_) => false,
-//     };
-//     let tracer_provider = if use_collector {
-//         let host = env::var("SAIL_OPENTELEMETRY_COLLECTOR_SERVICE_HOST")?;
-//         let port = env::var("SAIL_OPENTELEMETRY_COLLECTOR_SERVICE_PORT_OTLP_GRPC")?;
-//         let url = format!("http://{host}:{port}");
-//         opentelemetry_otlp::new_pipeline()
-//             .tracing()
-//             .with_exporter(
-//                 opentelemetry_otlp::new_exporter()
-//                     .tonic()
-//                     .with_endpoint(url)
-//                     .with_protocol(opentelemetry_otlp::Protocol::Grpc),
-//             )
-//             .with_batch_config(sdktrace::BatchConfig::default())
-//             .with_trace_config(trace_config)
-//             .install_batch(runtime::TokioCurrentThread)?
-//     } else {
-//         let processor =
-//             sdktrace::BatchSpanProcessor::builder(SpanExporter::default(), runtime::Tokio).build();
-//         sdktrace::TracerProvider::builder()
-//             .with_span_processor(processor)
-//             .with_config(trace_config)
-//             .build()
-//     };
-//     global::set_tracer_provider(tracer_provider.clone());
-//     Ok(tracer_provider.tracer("lakesail"))
-// }
-//
-// // TODO: init_metrics
-// pub fn init_metrics() {}
