0ffd4ba24fcba16bfdb8f849d9f00e7f26f72a82
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-10-08 23:33:35 -0700

    [spark tests]

--- crates/sail-spark-connect/src/session.rs
+++ crates/sail-spark-connect/src/session.rs
@@ -235,16 +235,30 @@ impl Session {
     }
 
     pub(crate) fn set_config(&self, kv: ConfigKeyValueList) -> SparkResult<()> {
+        use chrono::{Offset, Utc};
+        use chrono_tz::Tz;
         let mut state = self.state.lock()?;
         let kv: Vec<ConfigKeyValue> = kv.into();
         for ConfigKeyValue { key, value } in kv {
             if let Some(value) = value {
-                // FIXME: Parse timezone to format datafusion expects.
-                // if key == SPARK_SQL_SESSION_TIME_ZONE {
-                //     let state = self.context.state_ref();
-                //     let mut state = state.write();
-                //     state.config_mut().options_mut().execution.time_zone = Some(value.clone());
-                // }
+                if key == SPARK_SQL_SESSION_TIME_ZONE {
+                    let state = self.context.state_ref();
+                    let mut state = state.write();
+                    let tz: Tz = value
+                        .parse()
+                        .map_err(|e| SparkError::invalid(format!("invalid time zone: {e}")))?;
+                    let local_time_offset = Utc::now()
+                        .with_timezone(&tz)
+                        .offset()
+                        .fix()
+                        .local_minus_utc();
+                    let offset_string = format!(
+                        "{:+02}:{:02}",
+                        local_time_offset / 3600,
+                        (local_time_offset.abs() % 3600) / 60
+                    );
+                    state.config_mut().options_mut().execution.time_zone = Some(offset_string);
+                }
                 state.config.set(key, value)?;
             } else {
                 return Err(SparkError::invalid(format!(
