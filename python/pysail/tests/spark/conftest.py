95df9e5f0625fb203563a37c98e0cedef0b8f998
Heran Lin <heran@lakesail.com>
2024-10-22 17:44:25 +0800

    Set up unit tests for TPC-H and TPC-DS

--- python/pysail/tests/spark/conftest.py
+++ python/pysail/tests/spark/conftest.py
@@ -21,5 +21,10 @@ def remote():
 @pytest.fixture(scope="session", autouse=True)
 def spark(remote):
     spark = SparkSession.builder.remote(remote).getOrCreate()
+    # Set the Spark session time zone to UTC by default.
+    # Some test data (e.g. TPC-DS data) may generate timestamps that is invalid
+    # in some local time zones. This would result in `pytz.exceptions.NonExistentTimeError`
+    # when converting such timestamps from the local time zone to UTC.
+    spark.conf.set("spark.sql.session.timeZone", "UTC")
     yield spark
     spark.stop()
---
+++ python/pysail/tests/spark/test_tpcds.py
@@ -0,0 +1,43 @@
+from pathlib import Path
+
+import duckdb
+import pytest
+from pandas.testing import assert_frame_equal
+
+
+@pytest.fixture(scope="module")
+def duck():
+    conn = duckdb.connect()
+    conn.sql("CALL dsdgen(sf = 0.01)")
+    return conn
+
+
+@pytest.fixture(scope="module", autouse=True)
+def data(spark, duck):
+    tables = list(duck.sql("SHOW TABLES").df()["name"])
+    for table in tables:
+        df = duck.sql(f"SELECT * FROM {table}").df()  # noqa: S608
+        df = spark.createDataFrame(df)
+        df.createOrReplaceTempView(table)
+    yield
+    for table in tables:
+        spark.catalog.dropTempView(table)
+
+
+@pytest.mark.parametrize("query", [x + 1 for x in range(99)])
+@pytest.mark.skip(reason="TPC-DS queries are not yet fully supported")
+def test_tpcds_query(spark, duck, query):
+    path = Path(__file__).parent.parent.parent / "data" / "tpcds" / "queries" / f"q{query}.sql"
+    with open(path) as f:
+        text = f.read()
+    for sql in text.split(";"):
+        sql = sql.strip()  # noqa: PLW2901
+        if not sql:
+            continue
+        actual = spark.sql(sql)
+        # DuckDB and Spark has different syntax for quoted identifiers.
+        #   DuckDB: SELECT 1 AS "v"
+        #   Spark: SELECT 1 AS `v`
+        # In TPC-DS, quoted identifiers are used when column names contain spaces.
+        expected = duck.sql(sql.replace("`", '"'))
+        assert_frame_equal(actual.toPandas(), expected.df())
