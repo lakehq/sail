e4691c699ff292df22feba5f4b50dbbf2b711fa7
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-12-07 23:35:08 -0800

    save

--- crates/sail-common/src/spec/data_type.rs
+++ crates/sail-common/src/spec/data_type.rs
@@ -7,8 +7,7 @@ use serde::{Deserialize, Serialize};
 use crate::error::CommonError;
 
 /// Native Sail data types that convert to Arrow types.
-/// These types usually directly match to [arrow_schema::DataType] variants when there is a corresponding type.
-/// Exceptions to this are: [`Interval`].
+/// These types directly match to [arrow_schema::DataType] variants when there is a corresponding type.
 /// Additionally, custom data types are supported for cases not covered by Arrow (e.g. [`UserDefined`]).
 /// [Credit]: Comments within the enum are copied from [`arrow_schema::DataType`].
 #[derive(Debug, Clone, PartialEq, Eq, Hash, PartialOrd, Ord, Serialize, Deserialize)]
