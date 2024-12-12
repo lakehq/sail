ea9baa1d51835589909f78d289062aef387878ff
Heran Lin <heran@lakesail.com>
2024-12-12 22:18:03 +0800

    Update [spark tests]

--- python/pysail/tests/spark/test_table_function.py
+++ python/pysail/tests/spark/test_table_function.py
@@ -11,7 +11,11 @@ from pandas.testing import assert_frame_equal
         ("SELECT * FROM range(2, 5)", [2, 3, 4]),
         ("SELECT * FROM range(1 + 2)", [0, 1, 2]),
         ("SELECT * FROM range(CAST('2' AS INT))", [0, 1]),
+        ("SELECT * FROM range(3, 0, -1)", [1, 2, 3]),
+        ("SELECT * FROM range(10, 0, -2, 3)", [2, 4, 6, 8, 10]),
     ],
 )
 def test_range(sail, sql, expected):
-    assert_frame_equal(sail.sql(sql).toPandas(), pd.DataFrame({"id": expected}, dtype="int64"))
+    assert_frame_equal(
+        sail.sql(sql).toPandas().sort_values("id").reset_index(drop=True), pd.DataFrame({"id": expected}, dtype="int64")
+    )
