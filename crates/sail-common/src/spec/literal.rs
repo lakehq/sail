a2eddc8be82b86937d79e05708487df0333b490e
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-12-17 14:49:58 -0800

    Sail Literal

--- crates/sail-common/src/spec/literal.rs
+++ crates/sail-common/src/spec/literal.rs
@@ -1,62 +1,240 @@
+use datafusion::arrow::datatypes::i256;
+use half::f16;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
 use std::sync::Arc;
 
-use datafusion::arrow::datatypes::i256;
-use serde::{Deserialize, Deserializer, Serialize, Serializer};
-
-use crate::spec::DataType;
+use crate::spec;
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase", rename_all_fields = "camelCase")]
 pub enum Literal {
     Null,
-    Binary(Vec<u8>),
-    Boolean(bool),
-    Byte(i8),
-    Short(i16),
-    Integer(i32),
-    Long(i64),
-    Float(f32),
-    Double(f64),
-    Decimal128(Decimal128),
-    Decimal256(Decimal256),
-    String(String),
-    Date {
-        days: i32,
+    Boolean {
+        value: Option<bool>,
     },
-    // TODO: We need to implement the remaining Timestamp variants:
-    //  TimestampSecond, TimestampMillisecond, TimestampMicrosecond, TimestampNanosecond
-    TimestampMicrosecond {
-        microseconds: i64,
+    Int8 {
+        value: Option<i8>,
+    },
+    Int16 {
+        value: Option<i16>,
+    },
+    Int32 {
+        value: Option<i32>,
+    },
+    Int64 {
+        value: Option<i64>,
+    },
+    UInt8 {
+        value: Option<u8>,
+    },
+    UInt16 {
+        value: Option<u16>,
+    },
+    UInt32 {
+        value: Option<u32>,
+    },
+    UInt64 {
+        value: Option<u64>,
+    },
+    Float16 {
+        value: Option<f16>,
+    },
+    Float32 {
+        value: Option<f32>,
+    },
+    Float64 {
+        value: Option<f64>,
+    },
+    TimestampSecond {
+        seconds: Option<i64>,
         timezone: Option<Arc<str>>,
     },
-    TimestampNtz {
-        microseconds: i64,
+    TimestampMillisecond {
+        milliseconds: Option<i64>,
+        timezone: Option<Arc<str>>,
     },
-    CalendarInterval {
-        months: i32,
-        days: i32,
-        microseconds: i64,
+    TimestampMicrosecond {
+        microseconds: Option<i64>,
+        timezone: Option<Arc<str>>,
     },
-    YearMonthInterval {
-        months: i32,
+    TimestampNanosecond {
+        nanoseconds: Option<i64>,
+        timezone: Option<Arc<str>>,
     },
-    DayTimeInterval {
-        microseconds: i64,
+    TimestampNtzSecond {
+        seconds: Option<i64>,
     },
-    Array {
-        element_type: DataType,
-        elements: Vec<Literal>,
+    TimestampNtzMillisecond {
+        milliseconds: Option<i64>,
     },
-    Map {
-        key_type: DataType,
-        value_type: DataType,
-        keys: Vec<Literal>,
-        values: Vec<Literal>,
+    TimestampNtzMicrosecond {
+        microseconds: Option<i64>,
+    },
+    TimestampNtzNanosecond {
+        nanoseconds: Option<i64>,
+    },
+    Date32 {
+        days: Option<i32>,
+    },
+    Date64 {
+        milliseconds: Option<i64>,
+    },
+    Time32Second {
+        seconds: Option<i32>,
+    },
+    Time32Millisecond {
+        milliseconds: Option<i32>,
+    },
+    Time64Microsecond {
+        microseconds: Option<i64>,
+    },
+    Time64Nanosecond {
+        nanoseconds: Option<i64>,
+    },
+    DurationSecond {
+        seconds: Option<i64>,
+    },
+    DurationMillisecond {
+        milliseconds: Option<i64>,
+    },
+    DurationMicrosecond {
+        microseconds: Option<i64>,
+    },
+    DurationNanosecond {
+        nanoseconds: Option<i64>,
+    },
+    IntervalYearMonth {
+        months: Option<i32>,
+    },
+    IntervalDayTime {
+        days: Option<i32>,
+        milliseconds: Option<i32>,
+    },
+    IntervalMonthDayNano {
+        months: Option<i32>,
+        days: Option<i32>,
+        nanoseconds: Option<i64>,
+    },
+    Binary {
+        value: Option<Vec<u8>>,
+    },
+    FixedSizeBinary {
+        size: i32,
+        value: Option<Vec<u8>>,
+    },
+    LargeBinary {
+        value: Option<Vec<u8>>,
+    },
+    BinaryView {
+        value: Option<Vec<u8>>,
+    },
+    Utf8 {
+        value: Option<String>,
+    },
+    LargeUtf8 {
+        value: Option<String>,
+    },
+    Utf8View {
+        value: Option<String>,
+    },
+    List {
+        data_type: spec::DataType,
+        values: Arc<Vec<Literal>>,
+    },
+    FixedSizeList {
+        data_type: spec::DataType,
+        size: i32,
+        values: Arc<Vec<Literal>>,
+    },
+    LargeList {
+        data_type: spec::DataType,
+        values: Arc<Vec<Literal>>,
     },
     Struct {
-        struct_type: DataType,
-        elements: Vec<Literal>,
+        data_type: spec::DataType,
+        values: Arc<Vec<Literal>>,
     },
+    Union {
+        union_fields: spec::UnionFields,
+        union_mode: spec::UnionMode,
+    },
+    Dictionary {
+        key_type: Box<spec::DataType>,
+        value_type: Box<spec::DataType>,
+    },
+    Decimal128 {
+        precision: u8,
+        scale: i8,
+    },
+    Decimal256 {
+        precision: u8,
+        scale: i8,
+    },
+    Map {
+        key_type: Box<spec::DataType>,
+        value_type: Box<spec::DataType>,
+        value_type_nullable: bool,
+        keys_sorted: bool,
+    },
+    UserDefined {
+        jvm_class: Option<String>,
+        python_class: Option<String>,
+        serialized_python_class: Option<String>,
+        sql_type: Box<spec::DataType>,
+    },
+    ConfiguredUtf8 {
+        utf8_type: spec::Utf8Type,
+    },
+    ConfiguredBinary,
+    // Null,
+    // Binary(Vec<u8>),
+    // Boolean(bool),
+    // Byte(i8),
+    // Short(i16),
+    // Integer(i32),
+    // Long(i64),
+    // Float(f32),
+    // Double(f64),
+    // Decimal128(Decimal128),
+    // Decimal256(Decimal256),
+    // String(String),
+    // Date {
+    //     days: i32,
+    // },
+    // // TODO: We need to implement the remaining Timestamp variants:
+    // //  TimestampSecond, TimestampMillisecond, TimestampMicrosecond, TimestampNanosecond
+    // TimestampMicrosecond {
+    //     microseconds: i64,
+    //     timezone: Option<Arc<str>>,
+    // },
+    // TimestampNtz {
+    //     microseconds: i64,
+    // },
+    // CalendarInterval {
+    //     months: i32,
+    //     days: i32,
+    //     microseconds: i64,
+    // },
+    // YearMonthInterval {
+    //     months: i32,
+    // },
+    // DayTimeInterval {
+    //     microseconds: i64,
+    // },
+    // Array {
+    //     element_type: DataType,
+    //     elements: Vec<Literal>,
+    // },
+    // Map {
+    //     key_type: DataType,
+    //     value_type: DataType,
+    //     keys: Vec<Literal>,
+    //     values: Vec<Literal>,
+    // },
+    // Struct {
+    //     struct_type: DataType,
+    //     elements: Vec<Literal>,
+    // },
 }
 
 #[derive(Debug)]
