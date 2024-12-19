01ff05b370ffe7e2f09ce23eb9d7486832300d0d
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-12-18 22:34:48 -0800

    literal spec

--- crates/sail-common/src/spec/literal.rs
+++ crates/sail-common/src/spec/literal.rs
@@ -166,122 +166,23 @@ pub enum Literal {
     Decimal128 {
         precision: u8,
         scale: i8,
+        value: Option<i128>,
     },
     Decimal256 {
         precision: u8,
         scale: i8,
+        #[serde(
+            serialize_with = "serialize_i256",
+            deserialize_with = "deserialize_i256"
+        )]
+        value: Option<i256>,
     },
     Map {
         key_type: Box<spec::DataType>,
         value_type: Box<spec::DataType>,
-        value_type_nullable: bool,
-        keys_sorted: bool,
+        keys: Arc<Vec<Literal>>,
+        values: Arc<Vec<Literal>>,
     },
-    UserDefined {
-        jvm_class: Option<String>,
-        python_class: Option<String>,
-        serialized_python_class: Option<String>,
-        sql_type: Box<spec::DataType>,
-    },
-    ConfiguredUtf8 {
-        utf8_type: spec::Utf8Type,
-    },
-    ConfiguredBinary,
-    // Null,
-    // Binary(Vec<u8>),
-    // Boolean(bool),
-    // Byte(i8),
-    // Short(i16),
-    // Integer(i32),
-    // Long(i64),
-    // Float(f32),
-    // Double(f64),
-    // Decimal128(Decimal128),
-    // Decimal256(Decimal256),
-    // String(String),
-    // Date {
-    //     days: i32,
-    // },
-    // // TODO: We need to implement the remaining Timestamp variants:
-    // //  TimestampSecond, TimestampMillisecond, TimestampMicrosecond, TimestampNanosecond
-    // TimestampMicrosecond {
-    //     microseconds: i64,
-    //     timezone: Option<Arc<str>>,
-    // },
-    // TimestampNtz {
-    //     microseconds: i64,
-    // },
-    // CalendarInterval {
-    //     months: i32,
-    //     days: i32,
-    //     microseconds: i64,
-    // },
-    // YearMonthInterval {
-    //     months: i32,
-    // },
-    // DayTimeInterval {
-    //     microseconds: i64,
-    // },
-    // Array {
-    //     element_type: DataType,
-    //     elements: Vec<Literal>,
-    // },
-    // Map {
-    //     key_type: DataType,
-    //     value_type: DataType,
-    //     keys: Vec<Literal>,
-    //     values: Vec<Literal>,
-    // },
-    // Struct {
-    //     struct_type: DataType,
-    //     elements: Vec<Literal>,
-    // },
-}
-
-#[derive(Debug)]
-pub enum DecimalLiteral {
-    Decimal128(Decimal128),
-    Decimal256(Decimal256),
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Decimal128 {
-    pub value: i128,
-    pub precision: u8,
-    pub scale: i8,
-}
-
-impl Decimal128 {
-    pub fn new(value: i128, precision: u8, scale: i8) -> Self {
-        Self {
-            value,
-            precision,
-            scale,
-        }
-    }
-}
-
-#[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct Decimal256 {
-    #[serde(
-        serialize_with = "serialize_i256",
-        deserialize_with = "deserialize_i256"
-    )]
-    pub value: i256,
-    pub precision: u8,
-    pub scale: i8,
-}
-
-impl Decimal256 {
-    pub fn new(value: i256, precision: u8, scale: i8) -> Self {
-        Self {
-            value,
-            precision,
-            scale,
-        }
-    }
 }
 
 #[derive(Serialize, Deserialize)]
@@ -290,18 +191,23 @@ struct Int256 {
     high: i128,
 }
 
-fn serialize_i256<S>(value: &i256, serializer: S) -> Result<S::Ok, S::Error>
+fn serialize_i256<S>(value: &Option<i256>, serializer: S) -> Result<S::Ok, S::Error>
 where
     S: Serializer,
 {
-    let (low, high) = value.to_parts();
-    Int256 { low, high }.serialize(serializer)
+    match value {
+        Some(num) => {
+            let (low, high) = num.to_parts();
+            serializer.serialize_some(&Int256 { low, high })
+        }
+        None => serializer.serialize_none(),
+    }
 }
 
-fn deserialize_i256<'de, D>(deserializer: D) -> Result<i256, D::Error>
+fn deserialize_i256<'de, D>(deserializer: D) -> Result<Option<i256>, D::Error>
 where
     D: Deserializer<'de>,
 {
     let parts = Int256::deserialize(deserializer)?;
-    Ok(i256::from_parts(parts.low, parts.high))
+    Ok(Some(i256::from_parts(parts.low, parts.high)))
 }
