f6c4a0a8ea9bf6f00f3a896098573f4d2b013086
Shehab <11789402+shehabgamin@users.noreply.github.com>
2024-12-30 21:13:40 -0800

    [spark tests]

--- crates/sail-common/src/spec/literal.rs
+++ crates/sail-common/src/spec/literal.rs
@@ -2,6 +2,7 @@ use datafusion::arrow::datatypes::i256;
 use half::f16;
 use serde::{Deserialize, Deserializer, Serialize, Serializer};
 
+use crate::error::{CommonError, CommonResult};
 use crate::spec;
 
 /// See [`spec::DataType`] for details on datatypes
@@ -199,4 +200,167 @@ where
 {
     let parts = Int256::deserialize(deserializer)?;
     Ok(Some(i256::from_parts(parts.low, parts.high)))
+}
+
+pub fn data_type_to_null_literal(data_type: spec::DataType) -> CommonResult<Literal> {
+    match data_type {
+        spec::DataType::Null => Ok(Literal::Null),
+        spec::DataType::Boolean => Ok(Literal::Boolean { value: None }),
+        spec::DataType::Int8 => Ok(Literal::Int8 { value: None }),
+        spec::DataType::Int16 => Ok(Literal::Int16 { value: None }),
+        spec::DataType::Int32 => Ok(Literal::Int32 { value: None }),
+        spec::DataType::Int64 => Ok(Literal::Int64 { value: None }),
+        spec::DataType::UInt8 => Ok(Literal::UInt8 { value: None }),
+        spec::DataType::UInt16 => Ok(Literal::UInt16 { value: None }),
+        spec::DataType::UInt32 => Ok(Literal::UInt32 { value: None }),
+        spec::DataType::UInt64 => Ok(Literal::UInt64 { value: None }),
+        spec::DataType::Float16 => Ok(Literal::Float16 { value: None }),
+        spec::DataType::Float32 => Ok(Literal::Float32 { value: None }),
+        spec::DataType::Float64 => Ok(Literal::Float64 { value: None }),
+        spec::DataType::Timestamp {
+            time_unit,
+            timezone_info,
+        } => Ok({
+            match time_unit {
+                spec::TimeUnit::Second => Literal::TimestampSecond {
+                    seconds: None,
+                    timezone_info,
+                },
+                spec::TimeUnit::Millisecond => Literal::TimestampMillisecond {
+                    milliseconds: None,
+                    timezone_info,
+                },
+                spec::TimeUnit::Microsecond => Literal::TimestampMicrosecond {
+                    microseconds: None,
+                    timezone_info,
+                },
+                spec::TimeUnit::Nanosecond => Literal::TimestampNanosecond {
+                    nanoseconds: None,
+                    timezone_info,
+                },
+            }
+        }),
+        spec::DataType::Date32 => Ok(Literal::Date32 { days: None }),
+        spec::DataType::Date64 => Ok(Literal::Date64 { milliseconds: None }),
+        spec::DataType::Time32 { time_unit } => match time_unit {
+            spec::TimeUnit::Second => Ok(Literal::Time32Second { seconds: None }),
+            spec::TimeUnit::Millisecond => Ok(Literal::Time32Millisecond { milliseconds: None }),
+            spec::TimeUnit::Microsecond => Err(CommonError::invalid(
+                "DataType::Time32 to Literal::Time32 with TimeUnit::Microsecond",
+            )),
+            spec::TimeUnit::Nanosecond => Err(CommonError::invalid(
+                "DataType::Time32 to Literal::Time32 with TimeUnit::Nanosecond",
+            )),
+        },
+        spec::DataType::Time64 { time_unit } => match time_unit {
+            spec::TimeUnit::Second => Err(CommonError::invalid(
+                "DataType::Time64 to Literal::Time64 with TimeUnit::Second",
+            )),
+            spec::TimeUnit::Millisecond => Err(CommonError::invalid(
+                "DataType::Time64 to Literal::Time64 with TimeUnit::Millisecond",
+            )),
+            spec::TimeUnit::Microsecond => Ok(Literal::Time64Microsecond { microseconds: None }),
+            spec::TimeUnit::Nanosecond => Ok(Literal::Time64Nanosecond { nanoseconds: None }),
+        },
+        spec::DataType::Duration { time_unit } => match time_unit {
+            spec::TimeUnit::Second => Ok(Literal::DurationSecond { seconds: None }),
+            spec::TimeUnit::Millisecond => Ok(Literal::DurationMillisecond { milliseconds: None }),
+            spec::TimeUnit::Microsecond => Ok(Literal::DurationMicrosecond { microseconds: None }),
+            spec::TimeUnit::Nanosecond => Ok(Literal::DurationNanosecond { nanoseconds: None }),
+        },
+        spec::DataType::Interval {
+            interval_unit,
+            start_field: _,
+            end_field: _,
+        } => match interval_unit {
+            spec::IntervalUnit::YearMonth => Ok(Literal::IntervalYearMonth { months: None }),
+            spec::IntervalUnit::DayTime => Ok(Literal::IntervalDayTime {
+                days: None,
+                milliseconds: None,
+            }),
+            spec::IntervalUnit::MonthDayNano => Ok(Literal::IntervalMonthDayNano {
+                months: None,
+                days: None,
+                nanoseconds: None,
+            }),
+        },
+        spec::DataType::Binary => Ok(Literal::Binary { value: None }),
+        spec::DataType::FixedSizeBinary { size } => {
+            Ok(Literal::FixedSizeBinary { size, value: None })
+        }
+        spec::DataType::LargeBinary => Ok(Literal::LargeBinary { value: None }),
+        spec::DataType::BinaryView => Ok(Literal::BinaryView { value: None }),
+        spec::DataType::Utf8 => Ok(Literal::Utf8 { value: None }),
+        spec::DataType::LargeUtf8 => Ok(Literal::LargeUtf8 { value: None }),
+        spec::DataType::Utf8View => Ok(Literal::Utf8View { value: None }),
+        spec::DataType::List {
+            data_type,
+            nullable: _,
+        } => Ok(Literal::List {
+            data_type: *data_type,
+            values: None,
+        }),
+        spec::DataType::FixedSizeList {
+            data_type,
+            nullable: _,
+            length,
+        } => Ok(Literal::FixedSizeList {
+            length,
+            data_type: *data_type,
+            values: None,
+        }),
+        spec::DataType::LargeList {
+            data_type,
+            nullable: _,
+        } => Ok(Literal::LargeList {
+            data_type: *data_type,
+            values: None,
+        }),
+        spec::DataType::Struct { fields } => Ok(Literal::Struct {
+            data_type: spec::DataType::Struct { fields },
+            values: None,
+        }),
+        spec::DataType::Union {
+            union_fields,
+            union_mode,
+        } => Ok(Literal::Union {
+            union_fields,
+            union_mode,
+            value: None,
+        }),
+        spec::DataType::Dictionary {
+            key_type,
+            value_type,
+        } => Ok(Literal::Dictionary {
+            key_type: *key_type,
+            value_type: *value_type,
+            value: None,
+        }),
+        spec::DataType::Decimal128 { precision, scale } => Ok(Literal::Decimal128 {
+            precision,
+            scale,
+            value: None,
+        }),
+        spec::DataType::Decimal256 { precision, scale } => Ok(Literal::Decimal256 {
+            precision,
+            scale,
+            value: None,
+        }),
+        spec::DataType::Map {
+            key_type,
+            value_type,
+            value_type_nullable: _,
+            keys_sorted: _,
+        } => Ok(Literal::Map {
+            key_type: *key_type,
+            value_type: *value_type,
+            keys: None,
+            values: None,
+        }),
+        spec::DataType::ConfiguredUtf8 { .. } => Ok(Literal::Utf8 { value: None }),
+        spec::DataType::ConfiguredBinary => Ok(Literal::Binary { value: None }),
+        spec::DataType::UserDefined { .. } => Err(CommonError::NotSupported(
+            "DataType::UserDefined to Literal".to_string(),
+        )),
+    }
 }
