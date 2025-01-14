680ef3891fb821e2ce8c777a035ffd1104f82bc4
Heran Lin <heran@lakesail.com>
2025-01-14 13:06:07 +0800

    Update

--- python/pysail/tests/conftest.py
+++ python/pysail/tests/conftest.py
@@ -3,8 +3,16 @@ def pytest_configure(config):
 
     We include the tests in the installed package so that the user can test the installation
     via `pytest --pyargs pysail`.
-    We must override the configuration here instead of using `pytest.ini` or `pyproject.toml`
+    We must customize the configuration here instead of using `pytest.ini` or `pyproject.toml`
     since these files are not part of the installed package.
     """
+
+    # Note: configuration set via `config.inicfg` may not have an effect due to the cache used
+    # in `config.getini()`. In such a case, we may have to clear the INI cache in `config`.
+    # Since clearing the cache requires access to the private attribute of `config`, we do not
+    # do it here unless absolutely necessary, to avoid compatibility issues with future versions
+    # of pytest.
+    # In common cases, plugins only access the configuration for the first time after this hook,
+    # so the cache is not a problem.
+
     config.inicfg["doctest_optionflags"] = "ELLIPSIS NORMALIZE_WHITESPACE IGNORE_EXCEPTION_DETAIL NUMBER"
-    config._inicache.clear()  # noqa: SLF001
