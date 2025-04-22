883b1b3dc3cc9d8e67ed53904baa744f8f82a472
Heran Lin <heran@lakesail.com>
2025-04-22 14:29:03 +0800

    Fix

--- python/pysail/tests/spark/test_datetime.py
+++ python/pysail/tests/spark/test_datetime.py
@@ -41,7 +41,7 @@ from pysail.tests.spark.utils import strict
     ],
     indirect=["session_timezone", "local_timezone"],
 )
-@pytest.mark.skip(condition=platform.system() == "Windows", reason="`time.tzset()` is not available on Windows")
+@pytest.mark.skipif(platform.system() == "Windows", reason="`time.tzset()` is not available on Windows")
 def test_datetime_literal(sail, session_timezone, local_timezone, data):  # noqa: ARG001
     for dt, k, v in data:
         assert sail.range(1).select(lit(dt)).collect() == [strict(Row(**{f"TIMESTAMP '{k}'": v}))]
