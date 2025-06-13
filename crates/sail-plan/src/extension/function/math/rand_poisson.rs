9b727b96ee1205ae196c09113cad5dfa170a9631
David López <davidlghellin@gmail.com>
2025-06-13 16:46:55 +0200

    Basic ScalarUDFImpl for PoissonRand (think one test passed more)

---
+++ crates/sail-plan/src/extension/function/math/rand_poisson.rs
@@ -0,0 +1,65 @@
+use datafusion::arrow::array::{ArrayRef, Int64Array};
+use datafusion::arrow::datatypes::DataType;
+use datafusion_common::cast::as_int64_array;
+use datafusion_common::Result;
+use datafusion_expr::{ColumnarValue, ScalarFunctionArgs, ScalarUDFImpl, Signature, Volatility};
+use rand_distr::{Distribution, Poisson};
+use std::any::Any;
+use std::sync::Arc;
+
+#[derive(Debug)]
+pub struct PoissonRand {
+    signature: Signature,
+}
+
+impl Default for PoissonRand {
+    fn default() -> Self {
+        Self::new()
+    }
+}
+
+impl PoissonRand {
+    pub fn new() -> Self {
+        Self {
+            signature: Signature::uniform(1, vec![DataType::Float64], Volatility::Volatile),
+        }
+    }
+}
+
+impl ScalarUDFImpl for PoissonRand {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn name(&self) -> &str {
+        "poisson_rand"
+    }
+
+    fn signature(&self) -> &Signature {
+        &self.signature
+    }
+
+    fn return_type(&self, _arg_types: &[DataType]) -> Result<DataType> {
+        Ok(DataType::Int64)
+    }
+
+    fn invoke_with_args(&self, args: ScalarFunctionArgs) -> Result<ColumnarValue> {
+        let args: Vec<ArrayRef> = ColumnarValue::values_to_arrays(&args.args)?;
+        let i64s = as_int64_array(&args[0])?;
+
+        let mut rng = rand::rng();
+
+        let new_array = i64s
+            .iter()
+            .map(|array_elem| {
+                array_elem.map(|value| {
+                    Poisson::new(value as f64) // 🧠 convierte a f64 si es i64
+                        .unwrap()
+                        .sample(&mut rng) as i64
+                })
+            })
+            .collect::<Int64Array>();
+
+        Ok(ColumnarValue::from(Arc::new(new_array) as ArrayRef))
+    }
+}
