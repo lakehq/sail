21f5ef16b78ac2a5e8ac703536fd6bdd30c0d208
Rafael Fernández <github@rafaelfernandez.dev>
2025-06-14 16:46:46 +0200

    crafting SparkFromCSV UDF

--- crates/sail-plan/src/extension/function/csv/spark_from_csv.rs
+++ crates/sail-plan/src/extension/function/csv/spark_from_csv.rs
@@ -1,10 +1,16 @@
 use core::any::type_name;
 use datafusion::arrow::{
     array::{ArrayRef, StringArray},
-    datatypes::{DataType, Field, Fields},
+    datatypes::*,
 };
 use datafusion_common::{exec_err, DataFusionError, Result, ScalarValue};
-use datafusion_expr::{ColumnarValue, ScalarFunctionArgs, ScalarUDFImpl, Signature};
+use datafusion_expr::{
+    ColumnarValue, ReturnInfo, ReturnTypeArgs, ScalarFunctionArgs, ScalarUDFImpl, Signature,
+};
+
+use std::collections::HashMap;
+use std::io::Cursor;
+use std::sync::Arc;
 
 use crate::extension::function::functions_nested_utils::*;
 use crate::extension::function::functions_utils::make_scalar_function;
@@ -24,11 +30,10 @@ impl SparkFromCSV {
     pub fn new() -> Self {
         Self {
             // Because you could use it with either:
-            // - Two column expressions plus options
-            // - One column expression plus the struct DataType representing the schema plus options
-            // - One column expression plus the string representing the schema plus options
+            // - One column expression + the literal string representing the schema + options
+            // - One column expression + the literal string column with a string value representing the schema + options
             signature: Signature::variadic(
-                vec![DataType::Utf8],
+                vec![DataType::Utf8, DataType::Utf8View, DataType::LargeUtf8],
                 datafusion_expr::Volatility::Immutable,
             ),
         }
@@ -49,7 +54,13 @@ impl ScalarUDFImpl for SparkFromCSV {
     }
 
     fn return_type(&self, arg_types: &[DataType]) -> Result<DataType> {
+        // We cannot know the final DataType result without knowing the schema input args
         Ok(DataType::Struct(Fields::empty()))
+    }
+
+    fn return_type_from_args(&self, args: ReturnTypeArgs) -> Result<ReturnInfo> {
+        // We need to implement the return type related to the args
+        todo!()
     }
 
     fn invoke_with_args(&self, args: ScalarFunctionArgs) -> Result<ColumnarValue> {
@@ -58,10 +69,10 @@ impl ScalarUDFImpl for SparkFromCSV {
     }
 }
 
-pub fn spark_from_csv_inner(args: &[ArrayRef]) -> Result<ArrayRef> {
-    if args.len() != 3 {
+fn spark_from_csv_inner(args: &[ArrayRef]) -> Result<ArrayRef> {
+    if args.len() < 2 || args.len() > 3 {
         return exec_err!(
-            "`from_csv` function requires 3 arguments, got {}",
+            "`from_csv` function requires 2 or 3 arguments, got {}",
             args.len()
         );
     };
@@ -71,6 +82,7 @@ pub fn spark_from_csv_inner(args: &[ArrayRef]) -> Result<ArrayRef> {
     todo!()
 }
 
+// TODO: Double-check this implementation
 fn parse_schema_string(schema_str: &str, sep: &str) -> Result<Fields> {
     let fields: Result<Vec<Field>> = schema_str
         .split(sep)
@@ -80,16 +92,17 @@ fn parse_schema_string(schema_str: &str, sep: &str) -> Result<Fields> {
                 return exec_err!("Invalid field spec: '{}'", c);
             };
             let name: String = parts[0].to_string();
-            let dt: DataType = match parts[1].to_uppercase().as_str() {
-                "INT" | "INT32" => DataType::Int32,
-                "STRING" => DataType::Utf8,
-                "FLOAT" | "FLOAT64" => DataType::Float64,
-                // TODO. complete with all types. Is there any already built-in parser?
-                _ => return exec_err!("Unsupported type: {}", parts[1]),
-            };
-            Ok(Field::new(&name, dt, true))
+            let data_type: Result<DataType> = parse_data_type(&parts[1].to_uppercase().as_str());
+            data_type.map(|dt| Field::new(&name, dt, true))
         })
         .collect();
 
     fields.map(|f| Fields::from_iter(f))
+}
+// TODO: Is there any SQL type parser?
+// TODO: Double-check this implementation
+fn parse_data_type(raw_data_type: &str) -> Result<DataType> {
+    let dt: Result<DataType> = DataType::try_from(raw_data_type)
+        .map_err(|_e| DataFusionError::Internal(format!("Unsupported type: {}", raw_data_type)));
+    dt
 }
