06d2b8014b38f6cf9cda1982e1cb76f5bc44a289
David López <davidlghellin@gmail.com>
2025-06-17 19:25:29 +0200

    Revert "change param of test by spark"
    This reverts commit 6ef3439e21fc3c1dd699d28de5caa4848463551d.

--- python/pysail/tests/spark/test_sample.py
+++ python/pysail/tests/spark/test_sample.py
@@ -2,8 +2,8 @@ import pandas as pd
 from pandas.testing import assert_frame_equal
 
 
-def test_dataframe_sample_replazement_seed(spark):
-    df = spark.createDataFrame([(0), (1), (2), (3), (4), (5), (6), (7), (8), (9)], ["id"])
+def test_dataframe_sample_replazement_seed(sail):
+    df = sail.createDataFrame([(0), (1), (2), (3), (4), (5), (6), (7), (8), (9)], ["id"])
     df2 = df.sample(True, 0.5, 1)
 
     assert_frame_equal(
