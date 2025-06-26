dfb92a498356cf438f85d095e124a0f7a17f5900
Heran Lin <heran@lakesail.com>
2025-06-26 11:55:39 +0800

    Update

--- .github/workflows/release/get-previous-version.cjs
+++ .github/workflows/release/get-previous-version.cjs
@@ -21,7 +21,15 @@ module.exports = async ({ github, context, core }) => {
       }
       return patchB - patchA;
     });
-  const offset = context.event_name === "push" ? 1 : 0;
+  // We want to get the previous release version, excluding pre-releases.
+  // 1. For push events, if the tag is a normal release, the previous version
+  //    is the second item (offset 1) in the sorted list.
+  // 2. For push events, if the tag is a pre-release, the previous version
+  //    is the first item (offset 0) in the sorted list.
+  // 3. For other events (test release run), no new tag has been created yet,
+  //    and the previous version is the first item (offset 0) in the sorted list.
+  const offset =
+    context.event_name === "push" && pattern.test(context.ref_name) ? 1 : 0;
   if (versions.length <= offset) {
     core.setOutput("version", "");
   } else {
