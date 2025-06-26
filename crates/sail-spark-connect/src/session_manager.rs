e3a14909ca57c991bab091884fc89957e9b5789d
lonless9 <lonlessplus@gmail.com>
2025-06-26 19:17:54 +0800

    revert session

--- crates/sail-spark-connect/src/session_manager.rs
+++ crates/sail-spark-connect/src/session_manager.rs
@@ -172,15 +172,6 @@ impl SessionManager {
             .with_query_planner(new_query_planner())
             .build();
 
-        // Register Delta Lake table factory
-        state.table_factories_mut().insert(
-            "DELTA".to_string(),
-            Arc::new(sail_delta_lake::factory::DeltaTableFactory::new()),
-        );
-        state.table_factories_mut().insert(
-            "DELTALAKE".to_string(),
-            Arc::new(sail_delta_lake::factory::DeltaTableFactory::new()),
-        );
         let context = SessionContext::new_with_state(state);
 
         // TODO: This is a temp workaround to deregister all built-in functions that we define.
