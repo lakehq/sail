484c59dcf629a78409b3361b2ed8a4fe1f0b097d
lonless9 <lonlessplus@gmail.com>
2025-07-01 11:08:38 +0800

    data catalog

---
+++ crates/sail-delta-lake/src/data_catalog/storage/mod.rs
@@ -0,0 +1,155 @@
+//! listing_schema contains a SchemaProvider that scans ObjectStores for tables automatically
+use std::any::Any;
+use std::collections::{HashMap, HashSet};
+use std::path::Path;
+use std::sync::Arc;
+
+use async_trait::async_trait;
+use dashmap::DashMap;
+use datafusion::catalog::SchemaProvider;
+use datafusion::common::DataFusionError;
+use datafusion::datasource::TableProvider;
+use deltalake::errors::DeltaResult;
+use deltalake::logstore::{store_for, StorageConfig};
+use deltalake::open_table_with_storage_options;
+use deltalake::table::builder::ensure_table_uri;
+use futures::TryStreamExt;
+use object_store::ObjectStore;
+
+const DELTA_LOG_FOLDER: &str = "_delta_log";
+
+/// A `SchemaProvider` that scans an `ObjectStore` to automatically discover delta tables.
+///
+/// A subfolder relationship is assumed, i.e. given:
+/// authority = s3://host.example.com:3000
+/// path = /data/tpch
+///
+/// A table called "customer" will be registered for the folder:
+/// s3://host.example.com:3000/data/tpch/customer
+///
+/// assuming it contains valid deltalake data, i.e a `_delta_log` folder:
+/// s3://host.example.com:3000/data/tpch/customer/_delta_log/
+#[derive(Debug)]
+pub struct ListingSchemaProvider {
+    authority: String,
+    /// Underlying object store
+    store: Arc<dyn ObjectStore>,
+    /// A map of table names to a fully quilfied storage location
+    tables: DashMap<String, String>,
+    /// Options used to create underlying object stores
+    storage_options: StorageConfig,
+}
+
+impl ListingSchemaProvider {
+    /// Create a new [`ListingSchemaProvider`]
+    pub fn try_new(
+        root_uri: impl AsRef<str>,
+        options: Option<HashMap<String, String>>,
+    ) -> DeltaResult<Self> {
+        let uri = ensure_table_uri(root_uri)?;
+        let options = options.unwrap_or_default();
+        let store = store_for(&uri, &options)?;
+        Ok(Self {
+            authority: uri.to_string(),
+            store,
+            tables: DashMap::new(),
+            storage_options: StorageConfig::parse_options(options)?,
+        })
+    }
+
+    /// Reload table information from ObjectStore
+    pub async fn refresh(&self) -> datafusion::common::Result<()> {
+        let entries: Vec<_> = self.store.list(None).try_collect().await?;
+        let mut tables = HashSet::new();
+        for file in entries.iter() {
+            let mut parent = Path::new(file.location.as_ref());
+            while let Some(p) = parent.parent() {
+                if parent.ends_with(DELTA_LOG_FOLDER) {
+                    tables.insert(p);
+                    break;
+                }
+                parent = p;
+            }
+        }
+        for table in tables.into_iter() {
+            let table_name = normalize_table_name(table)?;
+            let table_path = table
+                .to_str()
+                .ok_or_else(|| DataFusionError::Internal("Cannot parse file name!".to_string()))?
+                .to_string();
+            if !self.table_exist(&table_name) {
+                let table_url = format!("{}/{table_path}", self.authority);
+                self.tables.insert(table_name.to_string(), table_url);
+            }
+        }
+        Ok(())
+    }
+}
+
+// normalizes a path fragment to be a valida table name in datafusion
+// - removes some reserved characters (-, +, ., " ")
+// - lowercase ascii
+fn normalize_table_name(path: &Path) -> Result<String, DataFusionError> {
+    Ok(path
+        .file_name()
+        .ok_or_else(|| DataFusionError::Internal("Cannot parse file name!".to_string()))?
+        .to_str()
+        .ok_or_else(|| DataFusionError::Internal("Cannot parse file name!".to_string()))?
+        .replace(['-', '.', ' '], "_")
+        .to_ascii_lowercase())
+}
+
+#[async_trait]
+impl SchemaProvider for ListingSchemaProvider {
+    fn as_any(&self) -> &dyn Any {
+        self
+    }
+
+    fn table_names(&self) -> Vec<String> {
+        self.tables.iter().map(|t| t.key().clone()).collect()
+    }
+
+    async fn table(
+        &self,
+        name: &str,
+    ) -> datafusion::common::Result<Option<Arc<dyn TableProvider>>> {
+        let Some(location) = self.tables.get(name).map(|t| t.clone()) else {
+            return Ok(None);
+        };
+        let delta_table =
+            open_table_with_storage_options(location, self.storage_options.raw.clone())
+                .await
+                .map_err(crate::delta_datafusion::delta_to_datafusion_error)?;
+        let config = crate::delta_datafusion::DeltaScanConfig::default();
+        let provider = crate::delta_datafusion::DeltaTableProvider::try_new(
+            delta_table.state.clone().unwrap(),
+            delta_table.log_store().clone(),
+            config,
+        )
+        .map_err(crate::delta_datafusion::delta_to_datafusion_error)?;
+        Ok(Some(Arc::new(provider)))
+    }
+
+    fn register_table(
+        &self,
+        _name: String,
+        _table: Arc<dyn TableProvider>,
+    ) -> datafusion::common::Result<Option<Arc<dyn TableProvider>>> {
+        Err(DataFusionError::Execution(
+            "schema provider does not support registering tables".to_owned(),
+        ))
+    }
+
+    fn deregister_table(
+        &self,
+        _name: &str,
+    ) -> datafusion::common::Result<Option<Arc<dyn TableProvider>>> {
+        Err(DataFusionError::Execution(
+            "schema provider does not support deregistering tables".to_owned(),
+        ))
+    }
+
+    fn table_exist(&self, name: &str) -> bool {
+        self.tables.contains_key(name)
+    }
+}
--- crates/sail-delta-lake/src/lib.rs
+++ crates/sail-delta-lake/src/lib.rs
@@ -1,2 +1,3 @@
+pub mod data_catalog;
 pub mod delta_datafusion;
 pub mod operations;
