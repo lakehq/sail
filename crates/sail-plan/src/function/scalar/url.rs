9444078b9b0797b7fed59b42bb120d4edf3b26eb
anhvdq <anh.vdq@gmail.com>
2025-07-14 23:41:00 +1000

    Run tests and finalizing

--- crates/sail-plan/src/function/scalar/url.rs
+++ crates/sail-plan/src/function/scalar/url.rs
@@ -1,11 +1,14 @@
-use crate::function::common::ScalarFunction;
+use crate::{
+    extension::function::url::{parse_url::ParseUrl, url_decode::UrlDecode, url_encode::UrlEncode},
+    function::common::ScalarFunction,
+};
 
 pub(super) fn list_built_in_url_functions() -> Vec<(&'static str, ScalarFunction)> {
     use crate::function::common::ScalarFunctionBuilder as F;
 
     vec![
-        ("parse_url", F::unknown("parse_url")),
-        ("url_decode", F::unknown("url_decode")),
-        ("url_encode", F::unknown("url_encode")),
+        ("parse_url", F::udf(ParseUrl::new())),
+        ("url_decode", F::udf(UrlDecode::new())),
+        ("url_encode", F::udf(UrlEncode::new())),
     ]
 }
