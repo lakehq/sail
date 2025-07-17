38d27defe6d2ee2c783324c432be6b836d1c2c6d
Rafael Fernández <github@rafaelfernandez.dev>
2025-07-17 18:17:05 +0200

    edit parse_number

--- crates/sail-plan/src/extension/function/string/spark_to_number.rs
+++ crates/sail-plan/src/extension/function/string/spark_to_number.rs
@@ -14,8 +14,6 @@ use datafusion_expr::{ColumnarValue, ScalarFunctionArgs, ScalarUDFImpl, Signatur
 use datafusion_expr_common::signature::Volatility;
 use regex::Regex;
 
-use crate::extension::function::functions_nested_utils::*;
-
 #[derive(Debug)]
 pub struct SparkToNumber {
     signature: Signature,
@@ -23,6 +21,9 @@ pub struct SparkToNumber {
 
 impl SparkToNumber {
     pub const NAME: &'static str = "to_number";
+
+    pub const FORMAT_REGEX: &'static str =
+        r"^(MI|S)?(L|\$)?(?<numbers>[09G,]+)(?<decimals>[.D][09]+)?(L|\$)?(PR|MI|S)?$";
 
     pub fn new() -> Self {
         Self {
@@ -51,7 +52,7 @@ impl ScalarUDFImpl for SparkToNumber {
     }
 
     fn return_type(&self, arg_types: &[DataType]) -> datafusion_common::Result<DataType> {
-        Ok(DataType::Decimal128(38, 9)) // TODO: use precision and scale from arg_types
+        Ok(DataType::Decimal256(38, 9)) // TODO: use precision and scale from arg_types
     }
 
     fn invoke_with_args(
@@ -73,33 +74,101 @@ pub fn spark_to_number_inner(args: &[ArrayRef]) -> Result<ArrayRef> {
     }
     let values = downcast_arg!(&args[0], StringArray);
     let format: &str = downcast_arg!(&args[1], StringArray).value(0);
+    let (precision, scale) = if let Some(Some(first)) = values.iter().find(|value| value.is_some())
+    {
+        let parsed = ParsedNumber::try_from(first, format)?;
+        (parsed.precision, parsed.scale)
+    } else {
+        (38, 9)
+    };
 
-    let result = values
+    let array = values
         .iter()
         .map(|value| match value {
-            Some(value) => todo!(),
-            None => ScalarValue::Decimal128(None, 38, 9),
+            Some(value) => match ParsedNumber::try_from(value, format) {
+                Ok(parsed) => {
+                    ScalarValue::Decimal256(Some(parsed.value), parsed.precision, parsed.scale)
+                }
+                Err(e) => return exec_err!("{}", e),
+            },
+            None => ScalarValue::Decimal256(None, precision, scale),
         })
         .collect::<ArrayRef>();
-
-    todo!()
+    Ok(array)
 }
 
-pub fn parse_number(value: &str, format: &str) -> Result<ScalarValue> {
-    let format = format.trim().split('.').collect::<Vec<_>>();
-    if let [left, right] = format[..] {
-        todo!()
+pub struct ParsedNumber {
+    pub value: i256,
+    pub precision: u8,
+    pub scale: i8,
+}
+
+impl ParsedNumber {
+    pub fn try_from(value: &str, format: &str) -> Result<Self> {
+        if let Ok(ParsedNumber {
+            value,
+            precision,
+            scale,
+        }) = parse_number(value, format)
+        {
+            Ok(Self {
+                value,
+                precision,
+                scale,
+            })
+        } else {
+            Err(DataFusionError::Execution(format!(
+                "Failed to parse number '{value}' with format '{format}'"
+            )))
+        }
     }
-    todo!()
+}
+pub fn parse_number(value: &str, format: &str) -> Result<ParsedNumber> {
+    let regex = match_format_regex(format)?;
+    let captures = regex.captures(value).ok_or_else(|| {
+        DataFusionError::Execution(format!(
+            "Value '{value}' does not match the format '{format}'"
+        ))
+    })?;
+    let numbers = captures
+        .name("numbers")
+        .map(|m| m.as_str().trim())
+        .ok_or_else(|| {
+            DataFusionError::Execution(format!("Missing '{}' group in the format regex", "numbers"))
+        })?;
+
+    let decimals = captures.name("decimals").map(|m| m.as_str().trim());
+    let scale = decimals.map_or(0, |d| d.len()) as i8;
+    let precision = numbers.len() as u8 + scale as u8;
+
+    let value = if let Some(decimals) = decimals {
+        format!("{}.{}", numbers, decimals.replace(".", "").replace("D", ""))
+    } else {
+        numbers.to_string()
+    };
+
+    let value = value.parse::<i256>().map_err(|e| {
+        DataFusionError::Execution(format!(
+            "Failed to parse composed number '{value}' with format '{format}': {e}"
+        ))
+    });
+
+    value.map(|value| ParsedNumber {
+        value,
+        precision,
+        scale,
+    })
 }
 
+/// Parse a number from a format string
+/// It follows the same regex pattern as the documentation
+/// https://docs.databricks.com/aws/en/sql/language-manual/functions/to_number#syntax
 pub fn match_format_regex(format: &str) -> Result<Regex> {
     // Define the regex pattern
-    let pattern = r"^(?:MI|S)\$[09G,]*[.D][09]*\$(?:PR|MI|S)$";
 
     // Create a Regex instance
-    let regex =
-        Regex::new(pattern).map_err(|e| plan_datafusion_err!("Failed to compile regex: {}", e))?;
+    let regex = Regex::new(SparkToNumber::FORMAT_REGEX)
+        .map_err(|e| plan_datafusion_err!("Failed to compile regex: {}", e))?;
 
     // Check if the format matches the regex pattern
     if regex.is_match(format) {
