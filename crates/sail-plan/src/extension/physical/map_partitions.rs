51f654c0e617096c942fac5223a0be16cb8ea612
Heran Lin <heran@lakesail.com>
2025-07-28 22:02:56 +0800

    fix: avoid repartitioning child for PySpark UDTF

--- crates/sail-plan/src/extension/physical/map_partitions.rs
+++ crates/sail-plan/src/extension/physical/map_partitions.rs
@@ -71,6 +71,10 @@ impl ExecutionPlan for MapPartitionsExec {
         &self.properties
     }
 
+    fn benefits_from_input_partitioning(&self) -> Vec<bool> {
+        vec![false]
+    }
+
     fn children(&self) -> Vec<&Arc<dyn ExecutionPlan>> {
         vec![&self.input]
     }
