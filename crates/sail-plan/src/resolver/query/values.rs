4fe9043b477bed72ba390bdce4f73a4989169370
Heran Lin <heran@lakesail.com>
2025-08-19 21:43:03 +0800

    Update

--- crates/sail-plan/src/resolver/query/values.rs
+++ crates/sail-plan/src/resolver/query/values.rs
@@ -23,7 +23,7 @@ impl PlanResolver<'_> {
                 let value = self.resolve_expressions(value, &schema, state).await?;
                 results.push(value);
             }
-            let _changed_column_indices = resolve_values_nan_types(&mut results, &schema)?;
+            let _changed_column_indices = Self::resolve_values_nan_types(&mut results, &schema)?;
             Ok(results) as PlanResult<_>
         }
         .await?;
@@ -40,64 +40,66 @@ impl PlanResolver<'_> {
             Arc::new(plan),
         )?))
     }
-}
 
-fn resolve_values_nan_types(
-    values: &mut Vec<Vec<Expr>>,
-    schema: &DFSchemaRef,
-) -> PlanResult<HashSet<usize>> {
-    let mut nan_positions = HashSet::new();
-    for value in values.iter() {
-        value.iter().enumerate().for_each(|(idx, expr)| {
-            if let Expr::Cast(cast) = expr {
-                if let Expr::Literal(sv, _) = cast.expr.as_ref() {
-                    if let Some(true) = sv
-                        .try_as_str()
-                        .flatten()
-                        .map(|s| s.to_uppercase() == "NAN" && cast.data_type.is_numeric())
-                    {
-                        nan_positions.insert(idx);
+    fn resolve_values_nan_types(
+        values: &mut Vec<Vec<Expr>>,
+        schema: &DFSchemaRef,
+    ) -> PlanResult<HashSet<usize>> {
+        let mut nan_positions = HashSet::new();
+        for value in values.iter() {
+            value.iter().enumerate().for_each(|(idx, expr)| {
+                if let Expr::Cast(cast) = expr {
+                    if let Expr::Literal(sv, _) = cast.expr.as_ref() {
+                        if let Some(true) = sv
+                            .try_as_str()
+                            .flatten()
+                            .map(|s| s.to_uppercase() == "NAN" && cast.data_type.is_numeric())
+                        {
+                            nan_positions.insert(idx);
+                        }
                     }
                 }
-            }
-        });
-    }
-
-    for idx in nan_positions.clone() {
-        let override_types = values
-            .iter()
-            .map(|result| {
-                Ok(match result[idx].get_type(&schema)? {
-                    DataType::Utf8 | DataType::LargeUtf8 => DataType::Utf8,
-                    DataType::Float64 | DataType::Decimal128(..) | DataType::Decimal256(..) => {
-                        DataType::Float64
-                    }
-                    _ => DataType::Float32,
-                })
-            })
-            .collect::<Result<Vec<_>, PlanError>>()?;
-
-        let target_type = override_types
-            .iter()
-            .try_fold(false, |has_float64, t| match t {
-                DataType::Utf8 | DataType::LargeUtf8 => Err(PlanError::invalid(format!(
-                    "Found incompatible types in column number {idx:?}"
-                ))),
-                DataType::Float64 | DataType::Decimal128(..) | DataType::Decimal256(..) => Ok(true),
-                _ => Ok(has_float64),
-            })
-            .map(|has_float64| {
-                if has_float64 {
-                    DataType::Float64
-                } else {
-                    DataType::Float32
-                }
-            })?;
-
-        for value in &mut *values {
-            value[idx] = cast(value[idx].clone(), target_type.clone());
+            });
         }
-    }
 
-    Ok(nan_positions)
+        for idx in nan_positions.clone() {
+            let override_types = values
+                .iter()
+                .map(|result| {
+                    Ok(match result[idx].get_type(&schema)? {
+                        DataType::Utf8 | DataType::LargeUtf8 => DataType::Utf8,
+                        DataType::Float64 | DataType::Decimal128(..) | DataType::Decimal256(..) => {
+                            DataType::Float64
+                        }
+                        _ => DataType::Float32,
+                    })
+                })
+                .collect::<Result<Vec<_>, PlanError>>()?;
+
+            let target_type = override_types
+                .iter()
+                .try_fold(false, |has_float64, t| match t {
+                    DataType::Utf8 | DataType::LargeUtf8 => Err(PlanError::invalid(format!(
+                        "Found incompatible types in column number {idx:?}"
+                    ))),
+                    DataType::Float64 | DataType::Decimal128(..) | DataType::Decimal256(..) => {
+                        Ok(true)
+                    }
+                    _ => Ok(has_float64),
+                })
+                .map(|has_float64| {
+                    if has_float64 {
+                        DataType::Float64
+                    } else {
+                        DataType::Float32
+                    }
+                })?;
+
+            for value in &mut *values {
+                value[idx] = cast(value[idx].clone(), target_type.clone());
+            }
+        }
+
+        Ok(nan_positions)
+    }
 }
