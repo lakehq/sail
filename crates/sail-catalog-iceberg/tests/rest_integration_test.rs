8418df7af07edf77ad56ca6a0a876f30ec63658c
Shehab <11789402+shehabgamin@users.noreply.github.com>
2025-10-22 21:25:25 -0700

    [spark tests]

--- crates/sail-catalog-iceberg/tests/rest_integration_test.rs
+++ crates/sail-catalog-iceberg/tests/rest_integration_test.rs
@@ -2,7 +2,7 @@
 
 use std::sync::Arc;
 
-use sail_catalog::provider::{CatalogProvider, Namespace};
+use sail_catalog::provider::{CatalogProvider, CreateDatabaseOptions, Namespace};
 use sail_catalog_iceberg::apis::configuration::Configuration;
 use sail_catalog_iceberg::IcebergRestCatalogProvider;
 use sail_common::runtime::RuntimeHandle;
@@ -103,4 +103,322 @@ async fn test_get_non_exist_namespace() {
         result.unwrap_err(),
         sail_catalog::error::CatalogError::NotFound(_, _)
     ));
+}
+
+#[tokio::test]
+async fn test_get_namespace() {
+    let (catalog, _minio, _mc, _rest) = setup_catalog().await;
+
+    let namespace = Namespace::try_from(vec!["apple".to_string(), "ios".to_string()]).unwrap();
+    let properties = vec![
+        ("owner".to_string(), "Lake".to_string()),
+        ("community".to_string(), "Sail".to_string()),
+    ];
+
+    assert!(catalog.get_database(&namespace).await.is_err());
+
+    let created_db = catalog
+        .create_database(
+            &namespace,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties: properties.clone(),
+            },
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(created_db.database, Vec::<String>::from(namespace.clone()));
+    for (key, value) in &properties {
+        assert!(created_db
+            .properties
+            .iter()
+            .any(|(k, v)| k == key && v == value));
+    }
+
+    let get_db = catalog.get_database(&namespace).await.unwrap();
+    assert_eq!(get_db.database, vec![namespace.to_string()]); // CHECK HERE FIX UNIT TESTS
+    for (key, value) in &properties {
+        assert!(get_db
+            .properties
+            .iter()
+            .any(|(k, v)| k == key && v == value));
+    }
+}
+
+#[tokio::test]
+async fn test_list_namespace() {
+    let (catalog, _minio, _mc, _rest) = setup_catalog().await;
+
+    let ns1 =
+        Namespace::try_from(vec!["test_list_namespace".to_string(), "ios".to_string()]).unwrap();
+    let ns1_properties = vec![
+        ("owner".to_string(), "Lake".to_string()),
+        ("community".to_string(), "Sail".to_string()),
+    ];
+
+    let ns2 =
+        Namespace::try_from(vec!["test_list_namespace".to_string(), "macos".to_string()]).unwrap();
+    let ns2_properties = vec![
+        ("owner".to_string(), "Meow".to_string()),
+        ("community".to_string(), "Peow".to_string()),
+    ];
+
+    let parent = Namespace::try_from(vec!["test_list_namespace".to_string()]).unwrap();
+
+    assert!(catalog.list_databases(Some(&parent)).await.is_err());
+
+    catalog
+        .create_database(
+            &ns1,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties: ns1_properties,
+            },
+        )
+        .await
+        .unwrap();
+
+    catalog
+        .create_database(
+            &ns2,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties: ns2_properties,
+            },
+        )
+        .await
+        .unwrap();
+
+    let dbs = catalog.list_databases(Some(&parent)).await.unwrap();
+
+    assert_eq!(dbs.len(), 2);
+    assert!(dbs
+        .iter()
+        .any(|db| db.database == Vec::<String>::from(ns1.clone())));
+    assert!(dbs
+        .iter()
+        .any(|db| db.database == Vec::<String>::from(ns2.clone())));
+}
+
+#[tokio::test]
+async fn test_list_empty_namespace() {
+    let (catalog, _minio, _mc, _rest) = setup_catalog().await;
+
+    let ns_apple = Namespace::try_from(vec![
+        "test_list_empty_namespace".to_string(),
+        "apple".to_string(),
+    ])
+    .unwrap();
+    let properties = vec![
+        ("owner".to_string(), "Lake".to_string()),
+        ("community".to_string(), "Sail".to_string()),
+    ];
+
+    assert!(catalog.list_databases(Some(&ns_apple)).await.is_err());
+
+    catalog
+        .create_database(
+            &ns_apple,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties,
+            },
+        )
+        .await
+        .unwrap();
+
+    let dbs = catalog.list_databases(Some(&ns_apple)).await.unwrap();
+    assert!(dbs.is_empty());
+}
+
+#[tokio::test]
+async fn test_list_root_namespace() {
+    let (catalog, _minio, _mc, _rest) = setup_catalog().await;
+
+    let ns1 = Namespace::try_from(vec![
+        "test_list_root_namespace".to_string(),
+        "apple".to_string(),
+        "ios".to_string(),
+    ])
+    .unwrap();
+    let ns1_properties = vec![
+        ("owner".to_string(), "Lake".to_string()),
+        ("community".to_string(), "Sail".to_string()),
+    ];
+
+    let ns2 = Namespace::try_from(vec![
+        "test_list_root_namespace".to_string(),
+        "google".to_string(),
+        "android".to_string(),
+    ])
+    .unwrap();
+    let ns2_properties = vec![
+        ("owner".to_string(), "Meow".to_string()),
+        ("community".to_string(), "Peow".to_string()),
+    ];
+
+    let parent = Namespace::try_from(vec!["test_list_root_namespace".to_string()]).unwrap();
+
+    assert!(catalog.list_databases(Some(&parent)).await.is_err());
+
+    catalog
+        .create_database(
+            &ns1,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties: ns1_properties,
+            },
+        )
+        .await
+        .unwrap();
+
+    catalog
+        .create_database(
+            &ns2,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties: ns2_properties,
+            },
+        )
+        .await
+        .unwrap();
+
+    let dbs = catalog.list_databases(None).await.unwrap();
+    assert_eq!(dbs.len(), 1);
+    assert_eq!(
+        dbs[0].database,
+        vec!["test_list_root_namespace".to_string()]
+    );
+}
+
+#[tokio::test]
+async fn test_list_empty_multi_level_namespace() {
+    let (catalog, _minio, _mc, _rest) = setup_catalog().await;
+
+    let ns_apple = Namespace::try_from(vec![
+        "test_list_empty_multi_level_namespace".to_string(),
+        "a_a".to_string(),
+        "apple".to_string(),
+    ])
+    .unwrap();
+    let properties = vec![
+        ("owner".to_string(), "Lake".to_string()),
+        ("community".to_string(), "Sail".to_string()),
+    ];
+
+    assert!(catalog.list_databases(Some(&ns_apple)).await.is_err());
+
+    catalog
+        .create_database(
+            &ns_apple,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties,
+            },
+        )
+        .await
+        .unwrap();
+
+    let dbs = catalog.list_databases(Some(&ns_apple)).await.unwrap();
+    assert!(dbs.is_empty());
+}
+
+#[tokio::test]
+async fn test_create_namespace() {
+    let (catalog, _minio, _mc, _rest) = setup_catalog().await;
+
+    let namespace = Namespace::try_from(vec!["test_create_namespace".to_string()]).unwrap();
+
+    let created_db = catalog
+        .create_database(
+            &namespace,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: Some("test comment".to_string()),
+                location: Some("s3://bucket/path".to_string()),
+                properties: vec![("key1".to_string(), "value1".to_string())],
+            },
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(created_db.database, Vec::<String>::from(namespace.clone()));
+    assert_eq!(created_db.comment, Some("test comment".to_string()));
+    assert_eq!(created_db.location, Some("s3://bucket/path".to_string()));
+    assert!(created_db
+        .properties
+        .iter()
+        .any(|(k, v)| k == "key1" && v == "value1"));
+
+    let result = catalog
+        .create_database(
+            &namespace,
+            CreateDatabaseOptions {
+                if_not_exists: false,
+                comment: None,
+                location: None,
+                properties: vec![],
+            },
+        )
+        .await;
+    assert!(result.is_err());
+
+    let namespace2 = Namespace::try_from(vec!["test_create_namespace_2".to_string()]).unwrap();
+    let created_db2 = catalog
+        .create_database(
+            &namespace2,
+            CreateDatabaseOptions {
+                if_not_exists: true,
+                comment: None,
+                location: None,
+                properties: vec![],
+            },
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(
+        created_db2.database,
+        Vec::<String>::from(namespace2.clone())
+    );
+    assert_eq!(created_db2.comment, None);
+    assert_eq!(
+        created_db2.location,
+        Some("s3://icebergdata/demo/test_create_namespace_2".to_string())
+    );
+
+    let created_again = catalog
+        .create_database(
+            &namespace2,
+            CreateDatabaseOptions {
+                if_not_exists: true,
+                comment: Some("should be ignored".to_string()),
+                location: Some("should be ignored".to_string()),
+                properties: vec![("ignored".to_string(), "ignored".to_string())],
+            },
+        )
+        .await
+        .unwrap();
+
+    assert_eq!(created_again.database, Vec::<String>::from(namespace2));
+    assert_eq!(created_again.comment, None);
+    assert_eq!(
+        created_again.location,
+        Some("s3://icebergdata/demo/test_create_namespace_2".to_string())
+    );
 }
