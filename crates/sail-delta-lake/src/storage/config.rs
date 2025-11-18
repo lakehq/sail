3d08b483a92c6eb5dfb7ceccd8f21d9623688a12
xiaolong <xiaolong@lakesail.com>
2025-11-18 14:54:21 +0800

    update

--- crates/sail-delta-lake/src/storage/config.rs
+++ crates/sail-delta-lake/src/storage/config.rs
@@ -1,3 +1,21 @@
+// https://github.com/delta-io/delta-rs/blob/5575ad16bf641420404611d65f4ad7626e9acb16/LICENSE.txt
+//
+// Copyright (2020) QP Hou and a number of other contributors.
+// Portions Copyright (2025) LakeSail, Inc.
+// Modified in 2025 by LakeSail, Inc.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 use std::sync::Arc;
 
 use object_store::path::Path;
@@ -7,6 +25,7 @@ use url::Url;
 
 use crate::kernel::DeltaResult;
 
+// [Credit]: <https://github.com/delta-io/delta-rs/blob/5575ad16bf641420404611d65f4ad7626e9acb16/crates/core/src/logstore/config.rs>
 /// Minimal storage configuration used to decorate object stores with table prefixes.
 #[derive(Debug, Clone, Default)]
 pub struct StorageConfig;
