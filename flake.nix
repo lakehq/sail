9edfd5ebc8d3d9a7996d4f290b12df34d615fb8f
David López <hola@devel0pez.com>
2026-01-20 23:57:57 +0100

    sail prerquisites

--- flake.nix
+++ flake.nix
@@ -4,8 +4,6 @@
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-utils.url = "github:numtide/flake-utils";
-
-    # Reproducible Rust toolchains (nightly pinned by date)
     fenix.url = "github:nix-community/fenix";
   };
 
@@ -17,20 +15,19 @@
 
         isLinux = pkgs.stdenv.isLinux;
 
-        # Pin Rust nightly by date (reproducible; controlled by flake.lock + this date)
-        rustToolchain = fenix.packages.${system}.latest.toolchain;
-
-        # If you prefer "latest nightly pinned only by flake.lock", use:
-        # rustToolchain = fenix.packages.${system}.latest.toolchain;
+        rustStable = fenix.packages.${system}.stable.toolchain;
+        rustNightly = fenix.packages.${system}.latest.toolchain;
 
         py = pkgs.python313;
         pyp = pkgs.python313Packages;
+	py311 = pkgs.python311;
+
       in {
         devShells.default = pkgs.mkShell {
           buildInputs =
             (with pkgs; [
               # Shell utils commonly assumed by scripts
- 	      fzf
+              fzf
               bashInteractive
               coreutils
               findutils
@@ -41,16 +38,24 @@
               procps
               ripgrep
 
-              # Rust (nightly, reproducible)
-              rustToolchain
-	      cargo-nextest
-              # Some Rust build helpers often needed by crates
+              # Prereqs from Sail docs
+              protobuf   # protoc
+              nodejs_20
+              pnpm
+              zig
+              maturin
+
+              # Rust toolchains (stable available; nightly is default)
+              rustStable
+              rustNightly
+
+              cargo-nextest
               pkg-config
 
               # Java for Spark
               jdk17
 
-              # Tooling for hatch + uv (avoid pipx uv issues)
+              # Tooling for hatch + uv
               hatch
               uv
 
@@ -65,7 +70,6 @@
               openssl
             ])
             ++ lib.optionals isLinux [
-              # Needed on Linux for many binary Python wheels (pandas/pyarrow/etc.)
               pkgs.stdenv.cc.cc.lib
             ];
 
@@ -75,37 +79,34 @@
               export JAVA_HOME=${pkgs.jdk17}
 
               export PYTHONPATH="$PWD/python:$PYTHONPATH"
-	      export PYO3_PYTHON="$(which python)"
+              export PYO3_PYTHON="${py}/bin/python"
 
-
-              # Accept `cargo +nightly ...` even without rustup (we're already on nightly via Nix)
+              # Accept `cargo +nightly ...` even without rustup (already on nightly via Nix)
               cargo() {
                 if [ "$1" = "+nightly" ]; then
-                   shift
+                  shift
                 fi
                 command cargo "$@"
-              }  
+              }
 
               # Pretty powerline prompt (Sail ⛵)
               if [ -z "$_NIX_OLD_PS1" ]; then
-                 export _NIX_OLD_PS1="$PS1"
+                export _NIX_OLD_PS1="$PS1"
               fi
               export PS1="\[\e[48;5;24m\]\[\e[38;5;231m\]  ⛵ sail  \[\e[0m\] \[\e[38;5;75m\]\w\[\e[0m\] \$ "
+
               # Enable fzf keybindings (Ctrl-R, Ctrl-T, Alt-C)
-if [ -e "${pkgs.fzf}/share/fzf/key-bindings.bash" ]; then
-  source "${pkgs.fzf}/share/fzf/key-bindings.bash"
-fi
-
-if [ -e "${pkgs.fzf}/share/fzf/completion.bash" ]; then
-  source "${pkgs.fzf}/share/fzf/completion.bash"
-fi
-
-              ''
-              + lib.optionalString isLinux ''
-                export ARROW_LIB_DIR=${pkgs.arrow-cpp}/lib
-                export LD_LIBRARY_PATH=${py}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.arrow-cpp}/lib:${pkgs.openssl}/lib:$LD_LIBRARY_PATH
-              '';
-
+              if [ -e "${pkgs.fzf}/share/fzf/key-bindings.bash" ]; then
+                source "${pkgs.fzf}/share/fzf/key-bindings.bash"
+              fi
+              if [ -e "${pkgs.fzf}/share/fzf/completion.bash" ]; then
+                source "${pkgs.fzf}/share/fzf/completion.bash"
+              fi
+            ''
+            + lib.optionalString isLinux ''
+              export ARROW_LIB_DIR=${pkgs.arrow-cpp}/lib
+              export LD_LIBRARY_PATH=${py311}/lib:${py}/lib:${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.arrow-cpp}/lib:${pkgs.openssl}/lib:$LD_LIBRARY_PATH
+            '';
         };
       }
     );
