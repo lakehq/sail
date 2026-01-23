b6e1915dd705e4c40d4aeb16c513b7b835939a2a
David López <hola@devel0pez.com>
2026-01-23 08:02:31 +0100

    clean

--- python/pysail/tests/spark/function/test_features.py
+++ python/pysail/tests/spark/function/test_features.py
@@ -1,21 +1,10 @@
 from __future__ import annotations
 
-import os
-
 import pytest
 from pytest_bdd import scenarios
 
 from pysail.tests.spark.utils import is_jvm_spark
 
 pytestmark = pytest.mark.skipif(is_jvm_spark(), reason="Sail only")
-
-
-@pytest.fixture(scope="session")
-def remote():
-    if remote := os.environ.get("SPARK_REMOTE"):
-        return remote
-    port = os.environ.get("SPARK_TESTING_REMOTE_PORT", "50051")
-    return f"sc://localhost:{port}"
-
 
 scenarios("features")
