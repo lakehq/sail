ce915cb13bc5ea6d90a8032ddab43006836211f1
Kyle Kent <kyle.kent321@gmail.com>
2026-02-08 10:57:54 -0600

    scalar -> struct working

--- crates/sail-plan/src/function/scalar/json.rs
+++ crates/sail-plan/src/function/scalar/json.rs
@@ -1,8 +1,13 @@
+use std::sync::Arc;
+
+use arrow::array::{Array, ArrayRef, Float64Array, Int64Array, StructArray};
+use arrow::datatypes::Field;
 use datafusion::arrow::datatypes::DataType;
-use datafusion_common::{DataFusionError, ScalarValue};
-use datafusion_expr::{cast, expr, lit, when};
+use datafusion_common::{DataFusionError, ScalarValue, nested_struct};
+use datafusion_expr::{ColumnarValue, cast, expr, lit, when};
 use datafusion_functions::unicode::expr_fn as unicode_fn;
 use datafusion_functions_json::udfs;
+use serde_json::Value;
 
 use crate::error::PlanResult;
 use crate::function::common::ScalarFunction;
@@ -39,11 +44,53 @@ fn json_object_keys(json_data: expr::Expr) -> expr::Expr {
     udfs::json_object_keys_udf().call(vec![json_data])
 }
 
+fn from_json(json_expr: expr::Expr, _schema_expr: expr::Expr) -> PlanResult<expr::Expr> {
+    let json_str = match json_expr {
+        expr::Expr::Literal(ScalarValue::Utf8(Some(utf8)), _) => utf8,
+        _ => unimplemented!("Onwy utf8 avaiwable")
+    };
+    let value: Value = serde_json::from_str::<serde_json::Value>(json_str.as_str()).unwrap();
+    let struct_ = match value {
+        Value::Object(map) => {
+            map_to_struct(map)
+        },
+        _ => unimplemented!("Onwy maps impwemented"),
+    };
+    let s = ScalarValue::Struct(Arc::new(struct_));
+    dbg!(&s);
+    Ok(expr::Expr::Literal(s, None))
+}
+
+fn map_to_struct(map: serde_json::Map<String, Value>) -> StructArray {
+    let mut fields = Vec::new();
+    let mut arrays: Vec<ArrayRef> = Vec::new();
+    for (k, v) in map {
+        match v {
+            Value::Number(num) => {
+                if num.is_i64() {
+                    fields.push(Field::new(k, DataType::Int64, true));
+                    arrays.push(Arc::new(Int64Array::from(vec![Some(num.as_i64().unwrap())])));
+                } else {
+                    fields.push(Field::new(k, DataType::Float64, true));
+                    arrays.push(Arc::new(Float64Array::from(vec![Some(num.as_f64().unwrap())])));
+                }
+            },
+            Value::Object(map) => {
+                let nested_struct = map_to_struct(map);
+                fields.push(Field::new(k, DataType::Struct(nested_struct.fields().clone()), nested_struct.is_nullable()));
+                arrays.push(Arc::new(nested_struct));
+            }
+            _ => unimplemented!("not here yet")
+        }
+    };
+    StructArray::new(fields.into(), arrays, None)
+}
+
 pub(super) fn list_built_in_json_functions() -> Vec<(&'static str, ScalarFunction)> {
     use crate::function::common::ScalarFunctionBuilder as F;
 
     vec![
-        ("from_json", F::unknown("from_json")),
+        ("from_json", F::binary(from_json)),
         ("get_json_object", F::binary(get_json_object)),
         ("json_array_length", F::unary(json_array_length)),
         ("json_object_keys", F::unary(json_object_keys)),
@@ -51,4 +98,40 @@ pub(super) fn list_built_in_json_functions() -> Vec<(&'static str, ScalarFunctio
         ("schema_of_json", F::unknown("schema_of_json")),
         ("to_json", F::unknown("to_json")),
     ]
+}
+
+#[cfg(test)]
+mod tests {
+
+    use datafusion::prelude::Column;
+    use datafusion_common::Spans;
+
+    use super::*;
+
+    #[test]
+    fn test_utf8() {
+        let s = r#"
+            {
+                "a": 1,
+                "b": 2,
+                "c": {"z": 1}
+            }
+        "#;
+        let expr_ = expr::Expr::Literal(ScalarValue::Utf8(Some(s.to_string())), None);
+        from_json(expr_.clone(), expr_.clone()).unwrap();
+    }
+
+    #[test]
+    fn test_column() {
+        let col = Column {
+            relation: None,
+            name: "meh".to_string(),
+            spans: Spans::new()
+        };
+        let expr_ = expr::Expr::Column(col);
+        // meh not done with this - not sure how to get data out
+        cast(expr_.clone(), DataType::Utf8);
+        from_json(expr_.clone(), expr_.clone()).unwrap();
+    }
+
 }
