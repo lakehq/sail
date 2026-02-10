1eb89eb45b28d05aabb7eaf81c345e7ae2db1a9c
David López <hola@devel0pez.com>
2026-02-10 19:56:39 +0100

    clean [spark tests][ibis tests]

--- python/pysail/tests/spark/function/test_features.py
+++ python/pysail/tests/spark/function/test_features.py
@@ -1,12 +1,5 @@
 """BDD tests for DataFrame operations using pytest-bdd."""
 
-from __future__ import annotations
-
-import pytest
 from pytest_bdd import scenarios
-
-from pysail.tests.spark.utils import is_jvm_spark
-
-pytestmark = pytest.mark.skipif(is_jvm_spark(), reason="Sail only")
 
 scenarios("features")
