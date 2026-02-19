53bd920d267e266d75007b8ff2706395cab4e663
Kyle Kent <kyle.kent321@gmail.com>
2026-02-19 16:39:31 -0600

    omg i think tests pass - still needs major clean up

--- crates/sail-plan/src/function/scalar/json.rs
+++ crates/sail-plan/src/function/scalar/json.rs
@@ -1,7 +1,9 @@
 use std::sync::Arc;
 
-use arrow::array::{Array, ArrayRef, Decimal32Builder, Float64Array, Int32Builder, Int64Array, Int64Builder, StructArray};
-use arrow::buffer::NullBuffer;
+use arrow::array::{
+	ArrayRef, Decimal32Builder, Float32Builder, Float64Builder, Int32Builder, Int64Builder, ListArray, ListBuilder, StringBuilder, StructArray
+};
+use arrow::buffer::{NullBuffer, OffsetBuffer, ScalarBuffer};
 use arrow::datatypes::{Field, Fields, SchemaBuilder};
 use datafusion::arrow::datatypes::DataType;
 use datafusion_common::{DataFusionError, ScalarValue};
@@ -16,6 +18,8 @@ use crate::function::common::ScalarFunction;
 
 use sail_sql_analyzer::parser::parse_data_type;
 use sail_sql_parser::ast::data_type::DataType as AstDataType;
+
+use crate::resolver::data_type_helper::PlanResolver;
 
 fn get_json_object(expr: expr::Expr, path: expr::Expr) -> PlanResult<expr::Expr> {
     let paths: Vec<expr::Expr> = match path {
@@ -54,19 +58,8 @@ fn from_json(json_expr: expr::Expr, schema_expr: expr::Expr) -> PlanResult<expr:
         expr::Expr::Literal(ScalarValue::Utf8(Some(utf8)), _) => utf8,
         _ => unimplemented!("Onwy utf8 avaiwable")
     };
-    let schema_struct = match schema_expr {
-        expr::Expr::Literal(ScalarValue::Utf8(Some(utf8)), _) => {
-            let schema = utf8.as_str();
-            if let Ok(dt) = parse_data_type(schema) {
-                dt
-            } else {
-                parse_data_type(format!("struct<{schema}>").as_str())?
-            }
-        },
-        _ => unimplemented!("Onwy utf8 avaiwable")
-    };
     let value: Value = serde_json::from_str::<serde_json::Value>(json_str.as_str()).unwrap();
-    let fields = schema_struct_datatype_to_fields(schema_struct)?;
+    let fields = get_schema_expr_as_fields(schema_expr)?;
     //let num_rows = value.as_array().unwrap().len();
     let num_rows = 1;
     let mut field_builders = create_field_builders(&fields, num_rows)?;
@@ -96,7 +89,10 @@ fn finish_builder(builder: FieldBuilder) -> PlanResult<ArrayRef> {
         match builder {
             FieldBuilder::Int32(mut b) => Arc::new(b.finish()),
             FieldBuilder::Int64(mut b) => Arc::new(b.finish()),
+            FieldBuilder::Float32(mut b) => Arc::new(b.finish()),
+            FieldBuilder::Float64(mut b) => Arc::new(b.finish()),
             FieldBuilder::Decimal32(mut b) => Arc::new(b.finish()),
+            FieldBuilder::String(mut b) => Arc::new(b.finish()),
             FieldBuilder::Struct {
                 fields,
                 builders,
@@ -108,6 +104,21 @@ fn finish_builder(builder: FieldBuilder) -> PlanResult<ArrayRef> {
                     .collect::<PlanResult<Vec<_>>>()?;
                 let null_buf = NullBuffer::from(null_buffer);
                 Arc::new(StructArray::new(fields, nested_arrays, Some(null_buf)))
+            },
+            FieldBuilder::List {
+                field,
+                offsets,
+                builder,
+                null_buffer
+            } => {
+                let field_builder = *builder;
+                let array_ref = finish_builder(field_builder)?;
+                Arc::new(ListArray::new(
+                    field,
+                    OffsetBuffer::new(ScalarBuffer::from(offsets)),
+                    array_ref,
+                    Some(NullBuffer::from(null_buffer)),
+                ))
             }
         }
     )
@@ -123,7 +134,10 @@ fn append_field_value(
             match builder {
                 FieldBuilder::Int32(builder) => builder.append_null(),
                 FieldBuilder::Int64(builder) => builder.append_null(),
+                FieldBuilder::Float32(builder) => builder.append_null(),
+                FieldBuilder::Float64(builder) => builder.append_null(),
                 FieldBuilder::Decimal32(builder) => builder.append_null(),
+                FieldBuilder::String(builder) => builder.append_null(),
                 FieldBuilder::Struct {
                     builders: nested_builders,
                     null_buffer,
@@ -131,6 +145,14 @@ fn append_field_value(
                 } => {
                     null_buffer.push(false);
                     append_null_to_all_builders(nested_builders)
+                },
+                FieldBuilder::List {
+					offsets,
+					null_buffer,
+                    ..
+                } => {
+                    null_buffer.push(false);
+                    offsets.push(*offsets.last().unwrap());
                 }
             }
             return Ok(());
@@ -157,9 +179,34 @@ fn append_field_value(
                 b.append_null(); // Type mismatch
             }
         },
+        (FieldBuilder::Float32(b), DataType::Float32) => {
+            if let Some(f) = value.as_f64() {
+                if f >= f32::MIN as f64 && f <= f32::MAX as f64 {
+                    b.append_value(f as f32);
+                } else {
+                    b.append_null(); // Overflow
+                }
+            } else {
+                b.append_null(); // Type mismatch
+            }
+        },
+        (FieldBuilder::Float64(b), DataType::Float64) => {
+            if let Some(f) = value.as_f64() {
+                b.append_value(f);
+            } else {
+                b.append_null(); // Type mismatch
+            }
+        },
         (FieldBuilder::Decimal32(b), DataType::Decimal32(_, _)) => {
             if let Some(f) = value.as_i64() {
                 b.append_value(f as i32);
+            } else {
+                b.append_null();
+            }
+        },
+        (FieldBuilder::String(b), DataType::Utf8) => {
+            if let Some(s) = value.as_str() {
+                b.append_value(s);
             } else {
                 b.append_null();
             }
@@ -183,6 +230,30 @@ fn append_field_value(
                 append_null_to_all_builders(nested_builders);
             }
         },
+        (
+            FieldBuilder::List {
+                field,
+                offsets,
+                builder,
+                null_buffer
+            },
+            DataType::List(_)
+        ) => {
+            if let Some(arr) = value.as_array() {
+                null_buffer.push(true);
+                for val in arr {
+                    append_field_value(builder, field, Some(val))?;
+                };
+                dbg!(&offsets);
+                dbg!(&offsets.last());
+                let last = *offsets.last().unwrap();
+                offsets.push(last + arr.len() as i32);
+            } else {
+                null_buffer.push(false);
+                let last = *offsets.last().unwrap();
+                offsets.push(last);
+            }
+        }
         (_, other) => {
             return Err(PlanError::NotSupported(format!("Invalid type in json: {:?}", other)));
         }
@@ -195,7 +266,10 @@ fn append_null_to_all_builders(builders: &mut [FieldBuilder]) {
         match builder {
             FieldBuilder::Int32(b) => b.append_null(),
             FieldBuilder::Int64(b) => b.append_null(),
+            FieldBuilder::Float32(b) => b.append_null(),
+            FieldBuilder::Float64(b) => b.append_null(),
             FieldBuilder::Decimal32(b) => b.append_null(),
+            FieldBuilder::String(b) => b.append_null(),
             FieldBuilder::Struct {
                 builders: nested_builder,
                 null_buffer,
@@ -203,34 +277,41 @@ fn append_null_to_all_builders(builders: &mut [FieldBuilder]) {
             } => {
                 null_buffer.push(false);
                 append_null_to_all_builders(nested_builder);
+            },
+            FieldBuilder::List {
+				offsets,
+				builder,
+				null_buffer,
+                ..
+            } => {
+                null_buffer.push(false);
+                let last = *offsets.last().unwrap();
+                offsets.push(last);
+                append_null_to_all_builders(std::slice::from_mut(builder));
             }
         }
     }
 }
 
-fn schema_struct_datatype_to_fields(schema_struct: AstDataType) -> PlanResult<Fields> {
-    let dtype = from_ast_data_type(schema_struct.clone());
-    dbg!(dtype);
-    let mut field_builder = SchemaBuilder::new();
-    match schema_struct.clone() {
-        AstDataType::Struct(_struct_identifier, _lt, Some(fields), _gt) => {
-            for item in fields.items() {
-                let arrow_data_type = match &item.data_type {
-                    AstDataType::Struct(_struct_identifier, _lt, Some(_fields), _gt) => {
-                        let fields = schema_struct_datatype_to_fields(item.data_type.clone())?;
-                        DataType::Struct(Fields::from(fields))
-                    },
-                    other => ast_data_type_to_arrow(&other)?
-                };
-                let nullable = item.not_null.is_none();
-                field_builder.push(Field::new(&item.identifier.value, arrow_data_type, nullable))
+fn get_schema_expr_as_fields(schema_expr: expr::Expr) -> PlanResult<Fields> {
+    let schema_struct = match schema_expr {
+        expr::Expr::Literal(ScalarValue::Utf8(Some(utf8)), _) => {
+            let schema = utf8.as_str();
+            if let Ok(dt) = parse_data_type(schema) {
+                dt
+            } else {
+                parse_data_type(format!("struct<{schema}>").as_str())?
             }
         },
-        other => {
-            return Err(PlanError::invalid(format!("Expected struct got {:?}", other)));
-        }
+        other => return Err(PlanError::NotImplemented(format!("Not implemented expr parsing for type {other:?}")))
+    };
+    let sail_dtype = from_ast_data_type(schema_struct.clone())?;
+    let arrow_dtype = PlanResolver.resolve_data_type(&sail_dtype)?;
+    dbg!(&arrow_dtype);
+    match arrow_dtype {
+        DataType::Struct(fields) => Ok(fields),
+        other => Err(PlanError::NotImplemented(format!("Not implemented {other:?}")))
     }
-    Ok(field_builder.finish().fields)
 }
 
 fn ast_data_type_to_arrow(ast_data_type: &AstDataType) -> PlanResult<DataType> {
@@ -256,12 +337,21 @@ fn ast_data_type_to_arrow(ast_data_type: &AstDataType) -> PlanResult<DataType> {
 enum FieldBuilder {
     Int32(Int32Builder),
     Int64(Int64Builder),
+    Float32(Float32Builder),
+    Float64(Float64Builder),
     Decimal32(Decimal32Builder),
+    String(StringBuilder),
     Struct {
         fields: Fields,
         builders: Vec<FieldBuilder>,
-        null_buffer: Vec<bool>
-    }
+        null_buffer: Vec<bool>,
+    },
+    List {
+        field: Arc<Field>,
+        offsets: Vec<i32>,
+        builder: Box<FieldBuilder>,
+        null_buffer: Vec<bool>,
+    },
 }
 
 fn create_field_builders(fields: &Fields, capacity: usize) -> PlanResult<Vec<FieldBuilder>> {
@@ -270,18 +360,34 @@ fn create_field_builders(fields: &Fields, capacity: usize) -> PlanResult<Vec<Fie
         .map(|field| match field.data_type() {
             DataType::Int32 => Ok(FieldBuilder::Int32(Int32Builder::with_capacity(capacity))),
             DataType::Int64 => Ok(FieldBuilder::Int64(Int64Builder::with_capacity(capacity))),
+            DataType::Float32 => Ok(FieldBuilder::Float32(Float32Builder::with_capacity(capacity))),
+            DataType::Float64 => Ok(FieldBuilder::Float64(Float64Builder::with_capacity(capacity))),
             DataType::Decimal32(_, _) => Ok(FieldBuilder::Decimal32(Decimal32Builder::with_capacity(capacity))),
+            DataType::Utf8 => Ok(FieldBuilder::String(StringBuilder::with_capacity(capacity, capacity*16))),
             DataType::Struct(fields) => {
                 let builders = create_field_builders(fields, capacity)?;
                 Ok(FieldBuilder::Struct {
                     fields: fields.clone(),
                     builders: builders,
                     null_buffer: Vec::with_capacity(capacity)
+
                 })
-            }
+            },
+            DataType::List(field) => {
+                // TODO: allow passing in one field rather than Fields
+                let builder = create_field_builders(&Fields::from(vec![field.clone()]), capacity)?.pop().unwrap();
+                let mut offsets = Vec::with_capacity(capacity + 1);
+                offsets.push(0);
+                Ok(FieldBuilder::List {
+                    field: field.clone(),
+                    offsets: offsets,
+                    builder: Box::new(builder),
+                    null_buffer: Vec::with_capacity(capacity)
+                })
+            },
             other => {
                 return Err(PlanError::unsupported(format!("Unsupported json type: {:?}", other)))
-            }
+            },
         })
         .collect()
 }
@@ -318,7 +424,14 @@ mod tests {
         "#;
         let expr_ = expr::Expr::Literal(ScalarValue::Utf8(Some(s.to_string())), None);
         let schema = r#"a int, b double"#;
-        //let schema = r#"struct<a int, b int>"#;
+        let schema_expr = expr::Expr::Literal(ScalarValue::Utf8(Some(schema.to_string())), None);
+        from_json(expr_.clone(), schema_expr.clone()).unwrap();
+
+        let s = r#"
+            {"teacher": "Alice", "student": [{"name": "Bob", "rank": 1}, {"name": "Charlie", "rank": 2}]}
+        "#;
+        let expr_ = expr::Expr::Literal(ScalarValue::Utf8(Some(s.to_string())), None);
+        let schema = r#"STRUCT<teacher: STRING, student: ARRAY<STRUCT<name: STRING, rank: INT>>>"#;
         let schema_expr = expr::Expr::Literal(ScalarValue::Utf8(Some(schema.to_string())), None);
         from_json(expr_.clone(), schema_expr.clone()).unwrap();
     }
---
+++ crates/sail-plan/src/resolver/data_type_helper.rs
@@ -0,0 +1,307 @@
+use sail_common::spec;
+use sail_common::spec::{
+    SAIL_LIST_FIELD_NAME, SAIL_MAP_FIELD_NAME, SAIL_MAP_KEY_FIELD_NAME, SAIL_MAP_VALUE_FIELD_NAME,
+};
+
+use crate::error::{PlanError, PlanResult};
+
+use datafusion::arrow::datatypes as adt;
+
+use std::sync::Arc;
+use std::collections::HashMap;
+
+pub(crate) struct PlanResolver;
+
+impl PlanResolver {
+    //fn arrow_binary_type(&self, state: &mut PlanResolverState) -> adt::DataType {
+    //    if self.config.arrow_use_large_var_types && state.config().arrow_allow_large_var_types {
+    //        adt::DataType::LargeBinary
+    //    } else {
+    //        adt::DataType::Binary
+    //    }
+    //}
+
+    //fn arrow_string_type(&self, state: &mut PlanResolverState) -> adt::DataType {
+    //    if self.config.arrow_use_large_var_types && state.config().arrow_allow_large_var_types {
+    //        adt::DataType::LargeUtf8
+    //    } else {
+    //        adt::DataType::Utf8
+    //    }
+    //}
+
+    //pub fn resolve_data_type_for_plan(
+    //    &self,
+    //    data_type: &spec::DataType,
+    //) -> PlanResult<adt::DataType> {
+    //    let mut state = PlanResolverState::new();
+    //    self.resolve_data_type(data_type, &mut state)
+    //}
+
+    /// References:
+    ///   org.apache.spark.sql.util.ArrowUtils#toArrowType
+    ///   org.apache.spark.sql.connect.common.DataTypeProtoConverter
+    pub fn resolve_data_type(
+        &self,
+        data_type: &spec::DataType,
+    ) -> PlanResult<adt::DataType> {
+        use spec::DataType;
+
+        match data_type {
+            DataType::Null => Ok(adt::DataType::Null),
+            DataType::Boolean => Ok(adt::DataType::Boolean),
+            DataType::Int8 => Ok(adt::DataType::Int8),
+            DataType::Int16 => Ok(adt::DataType::Int16),
+            DataType::Int32 => Ok(adt::DataType::Int32),
+            DataType::Int64 => Ok(adt::DataType::Int64),
+            DataType::UInt8 => Ok(adt::DataType::UInt8),
+            DataType::UInt16 => Ok(adt::DataType::UInt16),
+            DataType::UInt32 => Ok(adt::DataType::UInt32),
+            DataType::UInt64 => Ok(adt::DataType::UInt64),
+            DataType::Float16 => Ok(adt::DataType::Float16),
+            DataType::Float32 => Ok(adt::DataType::Float32),
+            DataType::Float64 => Ok(adt::DataType::Float64),
+            DataType::Timestamp {
+                time_unit,
+                timestamp_type,
+            } => Ok(adt::DataType::Timestamp(
+                Self::resolve_time_unit(time_unit)?,
+                self.resolve_timezone(timestamp_type)?,
+            )),
+            DataType::Date32 => Ok(adt::DataType::Date32),
+            DataType::Date64 => Ok(adt::DataType::Date64),
+            DataType::Time32 { time_unit } => {
+                Ok(adt::DataType::Time32(Self::resolve_time_unit(time_unit)?))
+            }
+            DataType::Time64 { time_unit } => {
+                Ok(adt::DataType::Time64(Self::resolve_time_unit(time_unit)?))
+            }
+            DataType::Duration { time_unit } => {
+                Ok(adt::DataType::Duration(Self::resolve_time_unit(time_unit)?))
+            }
+            DataType::Interval {
+                interval_unit,
+                start_field: _,
+                end_field: _,
+            } => {
+                // TODO: Currently `start_field` and `end_field` is lost in translation.
+                //  This does not impact computation accuracy,
+                //  This may affect the display string in the `data_type_to_simple_string` function.
+                Ok(adt::DataType::Interval(Self::resolve_interval_unit(
+                    interval_unit,
+                )))
+            }
+            DataType::Binary => Ok(adt::DataType::Binary),
+            DataType::FixedSizeBinary { size } => Ok(adt::DataType::FixedSizeBinary(*size)),
+            DataType::LargeBinary => Ok(adt::DataType::LargeBinary),
+            DataType::BinaryView => Ok(adt::DataType::BinaryView),
+            DataType::Utf8 => Ok(adt::DataType::Utf8),
+            DataType::LargeUtf8 => Ok(adt::DataType::LargeUtf8),
+            DataType::Utf8View => Ok(adt::DataType::Utf8View),
+            DataType::List {
+                data_type,
+                nullable,
+            } => {
+                let field = spec::Field {
+                    name: SAIL_LIST_FIELD_NAME.to_string(),
+                    data_type: data_type.as_ref().clone(),
+                    nullable: *nullable,
+                    metadata: vec![],
+                };
+                Ok(adt::DataType::List(Arc::new(
+                    self.resolve_field(&field)?,
+                )))
+            }
+            DataType::FixedSizeList {
+                data_type,
+                nullable,
+                length,
+            } => {
+                let field = spec::Field {
+                    name: SAIL_LIST_FIELD_NAME.to_string(),
+                    data_type: data_type.as_ref().clone(),
+                    nullable: *nullable,
+                    metadata: vec![],
+                };
+                Ok(adt::DataType::FixedSizeList(
+                    Arc::new(self.resolve_field(&field)?),
+                    *length,
+                ))
+            }
+            DataType::LargeList {
+                data_type,
+                nullable,
+            } => {
+                let field = spec::Field {
+                    name: SAIL_LIST_FIELD_NAME.to_string(),
+                    data_type: data_type.as_ref().clone(),
+                    nullable: *nullable,
+                    metadata: vec![],
+                };
+                Ok(adt::DataType::LargeList(Arc::new(
+                    self.resolve_field(&field)?,
+                )))
+            }
+            DataType::Struct { fields } => {
+                Ok(adt::DataType::Struct(self.resolve_fields(fields)?))
+            }
+            DataType::Union {
+                union_fields,
+                union_mode,
+            } => {
+                let union_fields = union_fields
+                    .iter()
+                    .map(|(i, field)| Ok((*i, Arc::new(self.resolve_field(field)?))))
+                    .collect::<PlanResult<_>>()?;
+                Ok(adt::DataType::Union(
+                    union_fields,
+                    Self::resolve_union_mode(union_mode),
+                ))
+            }
+            DataType::Dictionary {
+                key_type,
+                value_type,
+            } => Ok(adt::DataType::Dictionary(
+                Box::new(self.resolve_data_type(key_type)?),
+                Box::new(self.resolve_data_type(value_type)?),
+            )),
+            DataType::Decimal128 { precision, scale } => {
+                Ok(adt::DataType::Decimal128(*precision, *scale))
+            }
+            DataType::Decimal256 { precision, scale } => {
+                Ok(adt::DataType::Decimal256(*precision, *scale))
+            }
+            DataType::Map {
+                key_type,
+                value_type,
+                value_type_nullable,
+                keys_sorted,
+            } => {
+                let fields = spec::Fields::from(vec![
+                    spec::Field {
+                        name: SAIL_MAP_KEY_FIELD_NAME.to_string(),
+                        data_type: *key_type.clone(),
+                        nullable: false,
+                        metadata: vec![],
+                    },
+                    spec::Field {
+                        name: SAIL_MAP_VALUE_FIELD_NAME.to_string(),
+                        data_type: *value_type.clone(),
+                        nullable: *value_type_nullable,
+                        metadata: vec![],
+                    },
+                ]);
+                Ok(adt::DataType::Map(
+                    Arc::new(adt::Field::new(
+                        SAIL_MAP_FIELD_NAME,
+                        adt::DataType::Struct(self.resolve_fields(&fields)?),
+                        false,
+                    )),
+                    *keys_sorted,
+                ))
+            }
+            DataType::ConfiguredUtf8 { utf8_type: _ } => {
+                // FIXME: Currently `length` and `utf8_type` is lost in translation.
+                //  This impacts accuracy if `spec::ConfiguredUtf8Type` is `VarChar` or `Char`.
+                //Ok(self.arrow_string_type(state))
+                Ok(adt::DataType::Utf8)
+            }
+            DataType::ConfiguredBinary => Err(PlanError::NotSupported("Conversion of ConfiguredBinary to Binary not supported".to_string())),
+            DataType::UserDefined { .. } => Err(PlanError::unsupported(
+                "user defined data type should only exist in a field",
+            )),
+        }
+    }
+
+    pub fn resolve_field(
+        &self,
+        field: &spec::Field,
+    ) -> PlanResult<adt::Field> {
+        let spec::Field {
+            name,
+            data_type,
+            nullable,
+            metadata,
+        } = field;
+        let mut metadata: HashMap<_, _> = metadata
+            .iter()
+            .map(|(k, v)| (format!("metadata.{k}"), v.to_string()))
+            .collect();
+        let data_type = match data_type {
+            spec::DataType::UserDefined {
+                jvm_class,
+                python_class,
+                serialized_python_class,
+                sql_type,
+            } => {
+                if let Some(jvm_class) = jvm_class {
+                    metadata.insert("udt.jvm_class".to_string(), jvm_class.to_string());
+                }
+                if let Some(python_class) = python_class {
+                    metadata.insert("udt.python_class".to_string(), python_class.to_string());
+                }
+                if let Some(serialized_python_class) = serialized_python_class {
+                    metadata.insert(
+                        "udt.serialized_python_class".to_string(),
+                        serialized_python_class.to_string(),
+                    );
+                }
+                sql_type
+            }
+            x => x,
+        };
+        Ok(
+            adt::Field::new(name, self.resolve_data_type(data_type)?, *nullable)
+                .with_metadata(metadata),
+        )
+    }
+
+    pub fn resolve_fields(
+        &self,
+        fields: &spec::Fields,
+    ) -> PlanResult<adt::Fields> {
+        let fields = fields
+            .into_iter()
+            .map(|f| self.resolve_field(f))
+            .collect::<PlanResult<Vec<_>>>()?;
+        Ok(adt::Fields::from(fields))
+    }
+
+    pub fn resolve_schema(
+        &self,
+        schema: spec::Schema,
+    ) -> PlanResult<adt::Schema> {
+        let fields = self.resolve_fields(&schema.fields)?;
+        Ok(adt::Schema::new(fields))
+    }
+
+    pub fn resolve_time_unit(time_unit: &spec::TimeUnit) -> PlanResult<adt::TimeUnit> {
+        match time_unit {
+            spec::TimeUnit::Second => Ok(adt::TimeUnit::Second),
+            spec::TimeUnit::Millisecond => Ok(adt::TimeUnit::Millisecond),
+            spec::TimeUnit::Microsecond => Ok(adt::TimeUnit::Microsecond),
+            spec::TimeUnit::Nanosecond => Ok(adt::TimeUnit::Nanosecond),
+        }
+    }
+
+    pub fn resolve_interval_unit(interval_unit: &spec::IntervalUnit) -> adt::IntervalUnit {
+        match interval_unit {
+            spec::IntervalUnit::YearMonth => adt::IntervalUnit::YearMonth,
+            spec::IntervalUnit::DayTime => adt::IntervalUnit::DayTime,
+            spec::IntervalUnit::MonthDayNano => adt::IntervalUnit::MonthDayNano,
+        }
+    }
+
+    pub fn resolve_union_mode(union_mode: &spec::UnionMode) -> adt::UnionMode {
+        match union_mode {
+            spec::UnionMode::Sparse => adt::UnionMode::Sparse,
+            spec::UnionMode::Dense => adt::UnionMode::Dense,
+        }
+    }
+
+    pub fn resolve_timezone(
+        &self,
+        _timestamp_type: &spec::TimestampType,
+    ) -> PlanResult<Option<Arc<str>>> {
+        Ok(None)
+    }
+}
