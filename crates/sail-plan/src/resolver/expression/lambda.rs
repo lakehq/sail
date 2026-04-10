31429614bb420478e3a4271444174284c4cf5c5a
David López <hola@devel0pez.com>
2026-04-10 23:25:31 +0200

    clean [spark tests][ibis tests]

--- crates/sail-plan/src/resolver/expression/lambda.rs
+++ crates/sail-plan/src/resolver/expression/lambda.rs
@@ -1,5 +1,4 @@
 use datafusion_common::DFSchemaRef;
-use log::info;
 use sail_common::spec;
 
 use crate::error::{PlanError, PlanResult};
@@ -10,29 +9,20 @@ use crate::resolver::PlanResolver;
 impl PlanResolver<'_> {
     pub(super) async fn resolve_expression_lambda_function(
         &self,
-        function: spec::Expr,
-        arguments: Vec<spec::UnresolvedNamedLambdaVariable>,
+        _function: spec::Expr,
+        _arguments: Vec<spec::UnresolvedNamedLambdaVariable>,
         _schema: &DFSchemaRef,
         _state: &mut PlanResolverState,
     ) -> PlanResult<NamedExpr> {
-        // DEBUG: Let's see what the lambda looks like
-        info!("=== LAMBDA FUNCTION RECEIVED ===");
-        info!("Arguments: {:?}", arguments);
-        info!("Function body: {:#?}", function);
-        info!("================================");
         Err(PlanError::todo("lambda function"))
     }
 
     pub(super) async fn resolve_expression_named_lambda_variable(
         &self,
-        variable: spec::UnresolvedNamedLambdaVariable,
+        _variable: spec::UnresolvedNamedLambdaVariable,
         _schema: &DFSchemaRef,
         _state: &mut PlanResolverState,
     ) -> PlanResult<NamedExpr> {
-        // DEBUG: Let's see what the variable looks like
-        info!("=== NAMED LAMBDA VARIABLE ===");
-        info!("Variable: {:?}", variable);
-        info!("=============================");
         Err(PlanError::todo("named lambda variable"))
     }
 }
