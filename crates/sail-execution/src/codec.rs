45b8b67226d5ba90e58cafd5c619f911f30bbb20
David López <hola@devel0pez.com>
2026-04-14 07:31:53 +0200

    clean codec

--- crates/sail-execution/src/codec.rs
+++ crates/sail-execution/src/codec.rs
@@ -1878,8 +1878,6 @@ impl PhysicalExtensionCodec for RemoteExecutionCodec {
             "is_variant_null" => Ok(Arc::new(ScalarUDF::from(SparkIsVariantNullUdf::new()))),
             "variant_to_json" => Ok(Arc::new(ScalarUDF::from(SparkVariantToJsonUdf::new()))),
             "parse_json" => Ok(Arc::new(ScalarUDF::from(SparkJsonToVariantUdf::new()))),
-            "to_variant_object" => Ok(Arc::new(ScalarUDF::from(SparkToVariantObjectUdf::new()))),
-            "variant_get" => Ok(Arc::new(ScalarUDF::from(SparkVariantGet::new(false)))),
             "try_variant_get" => Ok(Arc::new(ScalarUDF::from(SparkVariantGet::new(true)))),
             "schema_of_variant" => Ok(Arc::new(ScalarUDF::from(SparkSchemaOfVariantUdf::new()))),
             "random" | "rand" => Ok(Arc::new(ScalarUDF::from(Random::new()))),
@@ -2088,7 +2086,6 @@ impl PhysicalExtensionCodec for RemoteExecutionCodec {
             || node_inner.is::<SparkIsVariantNullUdf>()
             || node_inner.is::<SparkJsonToVariantUdf>()
             || node_inner.is::<SparkToVariantObjectUdf>()
-            || node_inner.is::<SparkVariantGet>()
             || node_inner.is::<SparkSchemaOfVariantUdf>()
             || node_inner.is::<SparkLastDay>()
             || node_inner.is::<SparkLuhnCheck>()
