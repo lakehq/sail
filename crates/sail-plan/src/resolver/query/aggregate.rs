8600f44bd0df6d8596fb94206304df03c89233d8
XL Liang <xiaolong@lakesail.com>
2026-06-01 17:37:58 +0800

    feat: support grouping_id aggregate function

--- crates/sail-plan/src/resolver/query/aggregate.rs
+++ crates/sail-plan/src/resolver/query/aggregate.rs
@@ -1,7 +1,10 @@
 use datafusion_common::tree_node::{Transformed, TransformedResult, TreeNode, TreeNodeRecursion};
-use datafusion_common::ScalarValue;
+use datafusion_common::{Column, DataFusionError, Result as DataFusionResult, ScalarValue};
 use datafusion_expr::utils::{expr_as_column_expr, find_aggregate_exprs};
-use datafusion_expr::{Expr, LogicalPlan, LogicalPlanBuilder, Volatility};
+use datafusion_expr::{
+    bitwise_and, bitwise_shift_right, cast, Aggregate, Expr, LogicalPlan, LogicalPlanBuilder,
+    Volatility,
+};
 use sail_common::spec;
 use sail_common_datafusion::utils::items::ItemTaker;
 use sail_python_udf::get_udf_display_name;
@@ -140,6 +143,17 @@ impl PlanResolver<'_> {
         state: &mut PlanResolverState,
     ) -> PlanResult<LogicalPlan> {
         let grouping = self.resolve_grouping_positions(grouping, &projections)?;
+        let has_grouping_set = grouping
+            .iter()
+            .any(|x| matches!(x.expr, Expr::GroupingSet(_)));
+        let grouping_exprs = Self::distinct_grouping_expressions(&grouping);
+        let projections = projections
+            .into_iter()
+            .map(|x| Self::rewrite_grouping_functions(x, &grouping_exprs, has_grouping_set))
+            .collect::<PlanResult<Vec<_>>>()?;
+        let having = having
+            .map(|having| Self::rewrite_grouping_expr(having, &grouping_exprs, has_grouping_set))
+            .transpose()?;
         let mut aggregate_candidates = projections
             .iter()
             .map(|x| x.expr.clone())
@@ -230,6 +244,180 @@ impl PlanResolver<'_> {
         Ok(LogicalPlanBuilder::from(plan)
             .project(projections)?
             .build()?)
+    }
+
+    fn distinct_grouping_expressions(grouping: &[NamedExpr]) -> Vec<Expr> {
+        grouping
+            .iter()
+            .flat_map(|x| match &x.expr {
+                Expr::GroupingSet(g) => g.distinct_expr().into_iter().cloned().collect(),
+                expr => vec![expr.clone()],
+            })
+            .collect()
+    }
+
+    fn rewrite_grouping_functions(
+        named_expr: NamedExpr,
+        grouping_exprs: &[Expr],
+        has_grouping_set: bool,
+    ) -> PlanResult<NamedExpr> {
+        let NamedExpr {
+            name,
+            expr,
+            metadata,
+        } = named_expr;
+        Ok(NamedExpr {
+            name,
+            expr: Self::rewrite_grouping_expr(expr, grouping_exprs, has_grouping_set)?,
+            metadata,
+        })
+    }
+
+    fn rewrite_grouping_expr(
+        expr: Expr,
+        grouping_exprs: &[Expr],
+        has_grouping_set: bool,
+    ) -> PlanResult<Expr> {
+        Ok(expr
+            .transform_down(|expr| {
+                if let Expr::AggregateFunction(function) = expr {
+                    match function.func.name() {
+                        "grouping" => Ok(Transformed::yes(Self::grouping_on_grouping_id(
+                            function,
+                            grouping_exprs,
+                            has_grouping_set,
+                        )?)),
+                        "grouping_id" => Ok(Transformed::yes(Self::grouping_id_on_grouping_id(
+                            function,
+                            grouping_exprs,
+                            has_grouping_set,
+                        )?)),
+                        _ => Ok(Transformed::no(Expr::AggregateFunction(function))),
+                    }
+                } else {
+                    Ok(Transformed::no(expr))
+                }
+            })
+            .data()?)
+    }
+
+    fn grouping_id_column() -> Expr {
+        Expr::Column(Column::from(Aggregate::INTERNAL_GROUPING_ID))
+    }
+
+    fn grouping_bitmask_literal(value: u64, grouping_expr_count: usize) -> Expr {
+        let value = if grouping_expr_count <= 8 {
+            ScalarValue::UInt8(Some(value as u8))
+        } else if grouping_expr_count <= 16 {
+            ScalarValue::UInt16(Some(value as u16))
+        } else if grouping_expr_count <= 32 {
+            ScalarValue::UInt32(Some(value as u32))
+        } else {
+            ScalarValue::UInt64(Some(value))
+        };
+        Expr::Literal(value, None)
+    }
+
+    fn format_grouping_exprs(exprs: &[Expr]) -> String {
+        exprs
+            .iter()
+            .map(|expr| expr.to_string())
+            .collect::<Vec<_>>()
+            .join(", ")
+    }
+
+    fn reject_grouping_clause(
+        function: &datafusion_expr::expr::AggregateFunction,
+    ) -> DataFusionResult<()> {
+        if function.params.distinct
+            || function.params.filter.is_some()
+            || !function.params.order_by.is_empty()
+            || function.params.null_treatment.is_some()
+        {
+            Err(DataFusionError::Plan(format!(
+                "invalid {} function clause",
+                function.func.name()
+            )))
+        } else {
+            Ok(())
+        }
+    }
+
+    fn grouping_on_grouping_id(
+        function: datafusion_expr::expr::AggregateFunction,
+        grouping_exprs: &[Expr],
+        has_grouping_set: bool,
+    ) -> DataFusionResult<Expr> {
+        Self::reject_grouping_clause(&function)?;
+        if !has_grouping_set {
+            return Err(DataFusionError::Plan(
+                "[UNSUPPORTED_GROUPING_EXPRESSION] grouping functions can only be used with grouping sets, cube, or rollup".to_string(),
+            ));
+        }
+        let args = &function.params.args;
+        let [arg] = args.as_slice() else {
+            return Err(DataFusionError::Plan(
+                "grouping requires exactly one argument".to_string(),
+            ));
+        };
+        let position = grouping_exprs
+            .iter()
+            .position(|expr| expr == arg)
+            .ok_or_else(|| {
+                DataFusionError::Plan(format!(
+                    "[GROUPING_COLUMN_MISMATCH] grouping column {} is not in grouping columns ({})",
+                    arg,
+                    Self::format_grouping_exprs(grouping_exprs)
+                ))
+            })?;
+        let bitmap_index = grouping_exprs.len() - 1 - position;
+        let group_bit = bitwise_and(
+            Self::grouping_id_column(),
+            Self::grouping_bitmask_literal(1_u64 << bitmap_index, grouping_exprs.len()),
+        );
+        let shifted = if bitmap_index == 0 {
+            group_bit
+        } else {
+            bitwise_shift_right(
+                group_bit,
+                Self::grouping_bitmask_literal(bitmap_index as u64, grouping_exprs.len()),
+            )
+        };
+        Ok(cast(
+            shifted,
+            datafusion_common::arrow::datatypes::DataType::Int8,
+        ))
+    }
+
+    fn grouping_id_on_grouping_id(
+        function: datafusion_expr::expr::AggregateFunction,
+        grouping_exprs: &[Expr],
+        has_grouping_set: bool,
+    ) -> DataFusionResult<Expr> {
+        Self::reject_grouping_clause(&function)?;
+        if !has_grouping_set {
+            return Err(DataFusionError::Plan(
+                "[UNSUPPORTED_GROUPING_EXPRESSION] grouping functions can only be used with grouping sets, cube, or rollup".to_string(),
+            ));
+        }
+        if grouping_exprs.len() > 64 {
+            return Err(DataFusionError::Plan(
+                "[GROUPING_SIZE_LIMIT_EXCEEDED] grouping set size cannot be greater than 64"
+                    .to_string(),
+            ));
+        }
+        let args = &function.params.args;
+        if !args.is_empty() && args.as_slice() != grouping_exprs {
+            return Err(DataFusionError::Plan(format!(
+                "[GROUPING_ID_COLUMN_MISMATCH] grouping_id columns ({}) do not match grouping columns ({})",
+                Self::format_grouping_exprs(args),
+                Self::format_grouping_exprs(grouping_exprs)
+            )));
+        }
+        Ok(cast(
+            Self::grouping_id_column(),
+            datafusion_common::arrow::datatypes::DataType::Int64,
+        ))
     }
 
     /// Reference: [datafusion_sql::utils::rebase_expr]
