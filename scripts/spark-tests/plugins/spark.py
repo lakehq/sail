d2e01e7d22c9d8e2312f60daf71d272c8cfd6e49
pomykalakyle <pomykalakyle@gmail.com>
2026-06-17 19:27:58 -0700

    test: skip Spark collation doctest

--- scripts/spark-tests/plugins/spark.py
+++ scripts/spark-tests/plugins/spark.py
@@ -340,6 +340,13 @@ SKIPPED_SPARK_TESTS = [
         keywords=["pyspark.sql.functions.java_method"],
         reason="JVM-dependent test",
     ),
+    TestMarker(
+        keywords=["pyspark.sql.functions.builtin.collation"],
+        reason=(
+            "Spark string types carry collation metadata, but DataFusion/Arrow string types used by Sail do not "
+            "model Spark-style collations"
+        ),
+    ),
     # We skip all the streaming tests since some of them are slow,
     # and some of them test behaviors that are tied to the specific JVM implementation
     # of Spark Structured Streaming.
