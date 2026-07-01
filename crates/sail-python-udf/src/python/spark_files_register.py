f398952184c7e771848b42c72b896b607ddfed32
XL Liang <xiaolong@lakesail.com>
2026-07-01 13:35:37 +0800

    refactor: extract Python artifact helpers

---
+++ crates/sail-python-udf/src/python/spark_files_register.py
@@ -0,0 +1,17 @@
+import sys
+import types
+
+import pyspark
+
+
+files = sys.modules["pyspark.core.files"]
+core = sys.modules.get("pyspark.core")
+if core is None:
+    core = types.ModuleType("pyspark.core")
+    sys.modules["pyspark.core"] = core
+    pyspark.core = core
+
+core.files = files
+sys.modules["pyspark.files"] = files
+pyspark.files = files
+pyspark.SparkFiles = files.SparkFiles
