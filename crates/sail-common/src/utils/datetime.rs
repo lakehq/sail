8aa9c0b0e2904828d7675dcaad600151c9cb5df4
Shehab <11789402+shehabgamin@users.noreply.github.com>
2026-08-06 19:50:02 -0700

    add spark_timezone_parser func

--- crates/sail-common/src/utils/datetime.rs
+++ crates/sail-common/src/utils/datetime.rs
@@ -1,3 +1,6 @@
+use std::collections::HashMap;
+
+use arrow::array::timezone::Tz;
 use arrow::datatypes::TimeUnit;
 use iana_time_zone::get_timezone;
 
@@ -14,5 +17,67 @@ pub const fn time_unit_to_multiplier(time_unit: &TimeUnit) -> i64 {
         TimeUnit::Millisecond => 1000i64,
         TimeUnit::Microsecond => 1_000_000i64,
         TimeUnit::Nanosecond => 1_000_000_000i64,
+    }
+}
+
+pub fn spark_timezone_parser() -> impl Fn(Option<&str>) -> CommonResult<Option<Tz>> {
+    let legacy_timezones = HashMap::from([
+        ("ACT", "Australia/Darwin"),
+        ("AET", "Australia/Sydney"),
+        ("AGT", "America/Argentina/Buenos_Aires"),
+        ("ART", "Africa/Cairo"),
+        ("AST", "America/Anchorage"),
+        ("BET", "America/Sao_Paulo"),
+        ("BST", "Asia/Dhaka"),
+        ("CAT", "Africa/Harare"),
+        ("CNT", "America/St_Johns"),
+        ("CST", "America/Chicago"),
+        ("CTT", "Asia/Shanghai"),
+        ("EAT", "Africa/Addis_Ababa"),
+        ("ECT", "Europe/Paris"),
+        ("EST", "America/New_York"),
+        ("HST", "Pacific/Honolulu"),
+        ("IET", "America/Indianapolis"),
+        ("IST", "Asia/Calcutta"),
+        ("JST", "Asia/Tokyo"),
+        ("MIT", "Pacific/Apia"),
+        ("MST", "America/Denver"),
+        ("NET", "Asia/Yerevan"),
+        ("NST", "Pacific/Auckland"),
+        ("PLT", "Asia/Karachi"),
+        ("PNT", "America/Phoenix"),
+        ("PRT", "America/Puerto_Rico"),
+        ("PST", "America/Los_Angeles"),
+        ("SST", "Pacific/Guadalcanal"),
+        ("VST", "Asia/Saigon"),
+    ]);
+
+    move |input: Option<&str>| {
+        input
+            .map(|tz_str_opt| {
+                let tz_err = |tz_str| {
+                    Err(CommonError::invalid(format!(
+                        "[INVALID_TIMEZONE] The timezone: {tz_str:?} is invalid. \
+        The timezone must be either a region-based zone ID or a zone offset. \
+        Region IDs must have the form 'area/city', such as 'America/Los_Angeles'. \
+        Zone offsets must be in the format '(+|-)HH', '(+|-)HH:mm’ or '(+|-)HH:mm:ss', \
+        e.g '-08' , '+01:00' or '-13:33:33', and must be in the range from -18:00 to +18:00. \
+        'Z' and 'UTC' are accepted as synonyms for '+00:00'."
+                    )))
+                };
+
+                match tz_str_opt.parse::<Tz>() {
+                    Ok(tz) => Ok(Some(tz)),
+                    Err(_) => match legacy_timezones.get(tz_str_opt).cloned() {
+                        Some(tz_str) => match tz_str.parse::<Tz>() {
+                            Ok(tz) => Ok(Some(tz)),
+                            Err(_) => tz_err(tz_str),
+                        },
+                        None => tz_err(tz_str_opt),
+                    },
+                }
+            })
+            .transpose()
+            .map(|opt| opt.flatten())
     }
 }
