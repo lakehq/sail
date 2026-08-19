da99b88b46beab66ebc8c0b0e4e17c7997c5353e
XL Liang <xiaolong@lakesail.com>
2026-08-19 19:05:39 +0800

    test: remove sequence syntax unit test

--- crates/sail-sql-parser/tests/syntax.rs
+++ crates/sail-sql-parser/tests/syntax.rs
@@ -1,26 +1,6 @@
-use std::any::TypeId;
-
 use sail_common::tests::test_gold_set;
-use sail_sql_parser::ast::identifier::Ident;
-use sail_sql_parser::ast::operator::Comma;
 use sail_sql_parser::ast::statement::Statement;
-use sail_sql_parser::common::Sequence;
-use sail_sql_parser::tree::{SyntaxGraph, SyntaxNode, TreeSyntax};
-
-#[test]
-fn test_sequence_syntax_allows_single_item() {
-    let sequence_syntax = Sequence::<Ident, Comma>::syntax();
-    assert_eq!(
-        sequence_syntax.node,
-        SyntaxNode::Sequence(vec![
-            SyntaxNode::NonTerminal(TypeId::of::<Ident>()),
-            SyntaxNode::ZeroOrMore(Box::new(SyntaxNode::Sequence(vec![
-                SyntaxNode::NonTerminal(TypeId::of::<Comma>()),
-                SyntaxNode::NonTerminal(TypeId::of::<Ident>()),
-            ]))),
-        ])
-    );
-}
+use sail_sql_parser::tree::SyntaxGraph;
 
 #[test]
 #[expect(clippy::unwrap_used)]
