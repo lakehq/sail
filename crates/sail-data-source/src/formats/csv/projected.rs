fe83683f54e0483b645763adc43ec05fd68ccc7a
Shehab <11789402+shehabgamin@users.noreply.github.com>
2026-08-23 03:33:15 -0700

    lint

--- crates/sail-data-source/src/formats/csv/projected.rs
+++ crates/sail-data-source/src/formats/csv/projected.rs
@@ -497,6 +497,7 @@ impl<R: BufRead, D: Decoder> Iterator for DecoderBatchReader<R, D> {
     }
 }
 
+#[expect(clippy::unwrap_used)]
 #[cfg(test)]
 mod tests {
     use datafusion::arrow::csv::ReaderBuilder as ArrowReaderBuilder;
@@ -837,10 +838,7 @@ mod tests {
             ),
             (
                 true,
-                vec![
-                    vec![Some("a"), None],
-                    vec![Some("b;c"), Some("d")],
-                ],
+                vec![vec![Some("a"), None], vec![Some("b;c"), Some("d")]],
             ),
         ] {
             for chunk in [1, 2, 3, 64] {
