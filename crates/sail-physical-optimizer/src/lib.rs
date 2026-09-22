8318fe0624557f3920c01d1085cc64f32dcbdb5f
XL Liang <xiaolong@lakesail.com>
2026-09-22 17:22:43 +0800

    fix: stabilize aggregate pruning and CI expectations [tests]

--- crates/sail-physical-optimizer/src/lib.rs
+++ crates/sail-physical-optimizer/src/lib.rs
@@ -22,6 +22,7 @@ use datafusion::physical_optimizer::window_topn::WindowTopN;
 use crate::barrier::EnforceBarrierPartitioning;
 use crate::collect_left::RewriteCollectLeftHashJoin;
 use crate::explicit_repartition::RewriteExplicitRepartition;
+use crate::filter_pushdown::PostFilterPushdown;
 use crate::join_reorder::JoinReorder;
 pub use crate::join_reorder::JoinReorderOptions;
 use crate::projection_pushdown::LambdaSafeProjectionPushdown;
@@ -29,6 +30,7 @@ use crate::projection_pushdown::LambdaSafeProjectionPushdown;
 mod barrier;
 mod collect_left;
 mod explicit_repartition;
+mod filter_pushdown;
 mod join_reorder;
 mod projection_pushdown;
 
@@ -70,7 +72,7 @@ pub fn get_physical_optimizers(
     rules.push(Arc::new(LambdaSafeProjectionPushdown::new()));
     rules.push(Arc::new(PushdownSort::new()));
     rules.push(Arc::new(EnsureCooperative::new()));
-    rules.push(Arc::new(FilterPushdown::new_post_optimization()));
+    rules.push(Arc::new(PostFilterPushdown));
     rules.push(Arc::new(RewriteExplicitRepartition::new()));
     rules.push(Arc::new(RewriteCollectLeftHashJoin::new()));
     rules.push(Arc::new(EnforceBarrierPartitioning::new()));
